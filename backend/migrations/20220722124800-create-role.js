bb9eccd0abf498a12f5c878a3a3c56f24a61491f
cc-arindam <arindam.samanta@codeclouds.com>
2022-07-22 18:52:50 +0530

    Migrations upto permission role completed

---
+++ backend/migrations/20220722124800-create-role.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('roles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.BIGINT
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      slug: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_by: {
+        type: Sequelize.BIGINT
+      },
+      updated_by: {
+        type: Sequelize.BIGINT
+      },
+      deleted_by: {
+        type: Sequelize.BIGINT
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.TIME
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.TIME
+      },
+      deleted_at: {
+        type: Sequelize.TIME
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('roles');
+  }
+};
---
+++ backend/models/role.js
@@ -0,0 +1,33 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Role extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Role.init(
+    {
+      name: DataTypes.STRING,
+      slug: DataTypes.STRING,
+      created_by: DataTypes.BIGINT,
+      updated_by: DataTypes.BIGINT,
+      deleted_by: DataTypes.BIGINT,
+    },
+    {
+      sequelize,
+      modelName: "Role",
+      timestamps: true,
+      paranoid: true,
+      createdAt: "created_at", // alias createdAt as created_date
+      updatedAt: "updated_at",
+      deletedAt: "deleted_at",
+    }
+  );
+  return Role;
+};
