59dbabeddba3f1da302d3b9ef10950ccf68d0f4b
cc-sourabh <sourabh.das@codeclouds.in>
2022-08-02 17:10:09 +0530

    permission middleware

--- backend/middlewares/CheckPermissionMiddleware.js
+++ backend/middlewares/CheckPermissionMiddleware.js
@@ -1,9 +1,52 @@
-const { User, Group, Module, Permission } = require('../models')
+/**
+ * @description Middleware to check permissions of the user who sent the request. Request without enough privilage will result 403 error
+ * @author Sourabh (CodeClouds)
+ */
 
-class CheckPermissionMiddleware {
-  constructor() {
-    this.handle = this.handle.bind(this)
-  }
+// const rules = require("../config/acl");
+const extraActionsThatRequiresSamePermission = {
+  deleteParmanently: 'delete',
+  restore: 'delete',
+  change: 'update',
+  fetchDropdownOptions: 'list',
 }
 
-module.exports = CheckPermissionMiddleware
+function checkPermission(permissions, module, action) {
+  for (let permission of permissions) {
+    if (
+      ['deleteParmanently', 'restore'].indexOf(action) > -1 &&
+      permission.indexOf(`${module}-delete`) > -1
+    ) {
+      return true
+    } else if (
+      ['change'].indexOf(action) > -1 &&
+      permission.indexOf(`${module}-update`) > -1
+    ) {
+      return true
+    } else if (
+      ['fetchDropdownOptions'].indexOf(action) > -1 &&
+      permission.indexOf(`${module}-list`) > -1
+    ) {
+      return true
+    } else if (permission.indexOf(`${module}-${action}`) > -1) {
+      return true
+    }
+  }
+  return false
+}
+
+module.exports = function (req, res, next) {
+  const user = req.user
+  const company_id = 1
+  let partial_path = req.originalUrl.replace('api/', '').split('?').shift()
+  var [, module, action] = partial_path.split('/')
+  if (typeof action === 'undefined') {
+    action = 'list'
+  }
+  const companies = user.getCompanies({
+    where: {
+      company_id,
+    },
+  })
+  res.send(companies)
+}
--- backend/migrations/20220722104052-create-user.js
+++ backend/migrations/20220722104052-create-user.js
@@ -29,7 +29,7 @@ module.exports = {
       },
       password: {
         type: Sequelize.STRING,
-        allowNull: false,
+        allowNull: true,
       },
       phone_no: {
         type: Sequelize.STRING,
