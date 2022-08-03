3debe025689a9cba60305798562c4a7e439d07e5
cc-sourabh <sourabh.das@codeclouds.in>
2022-08-03 15:07:58 +0530

    Added companies in login service

--- backend/controllers/backend/AuthController.js
+++ backend/controllers/backend/AuthController.js
@@ -27,11 +27,11 @@ class AuthController {
       phone_no: Joi.string().required(),
       email: Joi.string().email().required(),
       invitation: Joi.number(),
-      token: Joi.string()
+      token: Joi.string(),
     })
     const { error, value } = schema.validate(req.body)
     if (error) {
-      let error_msg = error.details.map((err) => err.message);
+      let error_msg = error.details.map((err) => err.message)
       res.status(401).json({
         status: false,
         errors: error_msg.join(','),
@@ -39,27 +39,30 @@ class AuthController {
     }
 
     try {
-      
       let existing_user = await User.findAll({
         limit: 1,
         where: {
-          email: value.email
+          email: value.email,
         },
       })
-      
-      if ((existing_user.length > 0 && value.invitation =='') || (value.invitation == 1 && existing_user.length > 0 && existing_user[0].status == '1')) {
+
+      if (
+        (existing_user.length > 0 && value.invitation == '') ||
+        (value.invitation == 1 &&
+          existing_user.length > 0 &&
+          existing_user[0].status == '1')
+      ) {
         return res.status(400).json({
           status: false,
           errors: 'User Already Exists',
         })
       }
-      let hash_obj = '';
-      if(value.invitation == '1')
-      {
+      let hash_obj = ''
+      if (value.invitation == '1') {
         hash_obj = Buffer.from(value.token, 'base64')
         hash_obj = hash_obj.toString('utf8')
         hash_obj = JSON.parse(hash_obj)
-        if(new Date(hash_obj.expired_at) < new Date()){
+        if (new Date(hash_obj.expired_at) < new Date()) {
           return res.status(400).json({
             status: false,
             errors: 'This link has been expired',
@@ -68,27 +71,29 @@ class AuthController {
       }
       const salt = await bcrypt.genSalt(10)
       const password = await bcrypt.hash(value.password, salt)
-      let new_user = [];
-      if(value.invitation == '1')
-      {
-        
-        
-        let update_user = await User.update({
-          first_name:value.first_name,
-          last_name:value.last_name,
-          username:value.username,
-          password:password,
-          phone_no:value.phone_no,
-          status:1,
-        },{
-          where: { id: hash_obj.id }
-        })
-        new_user = await User.findOne({where: {id:hash_obj.id}});
+      let new_user = []
+      if (value.invitation == '1') {
+        let update_user = await User.update(
+          {
+            first_name: value.first_name,
+            last_name: value.last_name,
+            username: value.username,
+            password: password,
+            phone_no: value.phone_no,
+            status: 1,
+          },
+          {
+            where: { id: hash_obj.id },
+          }
+        )
+        new_user = await User.findOne({ where: { id: hash_obj.id } })
 
         //update invitation accepted time
-        let update_invitation = await Invitation.update({accepted_on:new Date()},{where:{id:hash_obj.invitation_id}})
-        
-      }else{
+        let update_invitation = await Invitation.update(
+          { accepted_on: new Date() },
+          { where: { id: hash_obj.invitation_id } }
+        )
+      } else {
         new_user = await User.create({
           ...value,
           password,
@@ -102,7 +107,7 @@ class AuthController {
       res.status(200).json({
         status: true,
         user: new_user,
-        access_token: token
+        access_token: token,
       })
     } catch (err) {
       console.log(err.message)
@@ -122,7 +127,7 @@ class AuthController {
     })
     const { error, value } = schema.validate(req.body)
     if (error) {
-      let error_msg = error.details.map((err) => err.message);
+      let error_msg = error.details.map((err) => err.message)
       res.status(401).json({
         status: false,
         errors: error_msg.join(','),
@@ -150,9 +155,11 @@ class AuthController {
         id: user.id,
       },
     })
+    const companies = await user.getCompanies()
     res.status(200).json({
       status: true,
       user: user,
+      companies,
       access_token: token,
     })
   }
@@ -200,7 +207,7 @@ class AuthController {
     })
     const { error, value } = schema.validate(req.body)
     if (error) {
-      let error_msg = error.details.map((err) => err.message);
+      let error_msg = error.details.map((err) => err.message)
       res.status(401).json({
         status: false,
         errors: error_msg.join(','),
@@ -224,7 +231,7 @@ class AuthController {
       status: true,
       reset_link:
         process.env.CLIENT_ORIGIN + '/reset-password?hash=' + base64String,
-        message: 'Reset password mail has been sent to your email',
+      message: 'Reset password mail has been sent to your email',
     })
   }
   //reset password
@@ -242,7 +249,7 @@ class AuthController {
     hash_obj = JSON.parse(hash_obj)
 
     if (error) {
-      let error_msg = error.details.map((err) => err.message);
+      let error_msg = error.details.map((err) => err.message)
       res.status(401).json({
         status: false,
         errors: error_msg.join(','),
