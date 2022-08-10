db25d8f407c737c757f83466ad65ec6b8e5ae838
cc-arindam <arindam.samanta@codeclouds.com>
2022-08-10 19:07:10 +0530

    Role Permission for Listing completed

--- admin/src/@fuse/hooks/usePermission.js
+++ admin/src/@fuse/hooks/usePermission.js
@@ -1,8 +1,10 @@
+import { selectUserPermissions } from "app/store/userSlice";
 import { useCallback, useState } from "react";
+import { useSelector } from "react-redux";
 
 function usePermission(module) {
     const types = ['all', 'group', 'owner'];
-    const userPermissions = ['all-users-add', 'all-users-list', 'all-users-save', 'all-users-update', 'all-users-delete'];
+    const userPermissions = useSelector(selectUserPermissions);
     const hasPermission = (action, type = 'all') => {
         let permission = `${type}-${module}-${action}`;
         return userPermissions.includes(permission);
