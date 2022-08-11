68669f5a06b4778a2ede9eac99ebc0b07d58859b
cc-arindam <arindam.samanta@codeclouds.com>
2022-08-11 14:18:52 +0530

    Changes in userPermission

--- admin/src/@fuse/hooks/usePermission.js
+++ admin/src/@fuse/hooks/usePermission.js
@@ -1,10 +1,9 @@
-import { selectUserPermissions } from "app/store/userSlice";
 import { useCallback, useState } from "react";
 import { useSelector } from "react-redux";
 
 function usePermission(module) {
     const types = ['all', 'group', 'owner'];
-    const userPermissions = useSelector(selectUserPermissions);
+    const userPermissions = useSelector(state=>state.user.permissions);
     const hasPermission = (action, type = 'all') => {
         let permission = `${type}-${module}-${action}`;
         return userPermissions.includes(permission);
