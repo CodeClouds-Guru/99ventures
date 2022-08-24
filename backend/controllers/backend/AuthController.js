f8fd64dc585c7b022e0fdf5abad3f6f9ba49b515
CODECLOUDS-DEBOSMITA <codeclouds-debosmita@CISPL-IO-659.local>
2022-08-24 16:48:24 +0530

    commit for resolving conflicts in authcontroller

--- backend/controllers/backend/AuthController.js
+++ backend/controllers/backend/AuthController.js
@@ -3,8 +3,8 @@
  * @author Sourabh (CodeClouds)
  */
 
-const Joi = require('joi')
-const { User } = require('../../models/index')
+const Joi = require("joi");
+const { User } = require("../../models/index");
 const {
   Invitation,
   GroupRole,
@@ -13,21 +13,21 @@ const {
   Permission,
   Group,
   Company,
-} = require('../../models/index')
-const { QueryTypes, Op } = require('sequelize')
-const db = require('../../models/index')
-const bcrypt = require('bcryptjs')
-const { generateToken } = require('../../helpers/global')
-const permission = require('../../models/permission')
-const UserResources = require('../../resources/UserResources')
+} = require("../../models/index");
+const { QueryTypes, Op } = require("sequelize");
+const db = require("../../models/index");
+const bcrypt = require("bcryptjs");
+const { generateToken } = require("../../helpers/global");
+const permission = require("../../models/permission");
+const UserResources = require("../../resources/UserResources");
 
 class AuthController {
   constructor() {
-    this.signup = this.signup.bind(this)
+    this.signup = this.signup.bind(this);
     // this.validate = this.validate.bind(this)
-    this.getCompanyAndSites = this.getCompanyAndSites.bind(this)
-    this.login = this.login.bind(this)
-    this.refreshToken = this.refreshToken.bind(this)
+    this.getCompanyAndSites = this.getCompanyAndSites.bind(this);
+    this.login = this.login.bind(this);
+    this.refreshToken = this.refreshToken.bind(this);
   }
 
   async signup(req, res) {
@@ -36,20 +36,20 @@ class AuthController {
       last_name: Joi.string().required(),
       username: Joi.string().alphanum().min(3).max(30).required(),
       password: Joi.string()
-        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+        .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
         .required(),
       phone_no: Joi.string().required(),
       email: Joi.string().email().required(),
       invitation: Joi.number(),
       token: Joi.string(),
-    })
-    const { error, value } = schema.validate(req.body)
+    });
+    const { error, value } = schema.validate(req.body);
     if (error) {
-      let error_msg = error.details.map((err) => err.message)
+      let error_msg = error.details.map((err) => err.message);
       res.status(401).json({
         status: false,
-        errors: error_msg.join(','),
-      })
+        errors: error_msg.join(","),
+      });
     }
 
     try {
@@ -58,35 +58,35 @@ class AuthController {
         where: {
           email: value.email,
         },
-      })
+      });
 
       if (
-        (existing_user.length > 0 && value.invitation == '') ||
+        (existing_user.length > 0 && value.invitation == "") ||
         (value.invitation == 1 &&
           existing_user.length > 0 &&
-          existing_user[0].status == '1')
+          existing_user[0].status == "1")
       ) {
         return res.status(400).json({
           status: false,
-          errors: 'User Already Exists',
-        })
+          errors: "User Already Exists",
+        });
       }
-      let hash_obj = ''
-      if (value.invitation == '1') {
-        hash_obj = Buffer.from(value.token, 'base64')
-        hash_obj = hash_obj.toString('utf8')
-        hash_obj = JSON.parse(hash_obj)
+      let hash_obj = "";
+      if (value.invitation == "1") {
+        hash_obj = Buffer.from(value.token, "base64");
+        hash_obj = hash_obj.toString("utf8");
+        hash_obj = JSON.parse(hash_obj);
         if (new Date(hash_obj.expired_at) < new Date()) {
           return res.status(400).json({
             status: false,
-            errors: 'This link has been expired',
-          })
+            errors: "This link has been expired",
+          });
         }
       }
-      const salt = await bcrypt.genSalt(10)
-      const password = await bcrypt.hash(value.password, salt)
-      let new_user = []
-      if (value.invitation == '1') {
+      const salt = await bcrypt.genSalt(10);
+      const password = await bcrypt.hash(value.password, salt);
+      let new_user = [];
+      if (value.invitation == "1") {
         let update_user = await User.update(
           {
             first_name: value.first_name,
@@ -99,80 +99,80 @@ class AuthController {
           {
             where: { id: hash_obj.id },
           }
-        )
-        new_user = await User.findOne({ where: { id: hash_obj.id } })
+        );
+        new_user = await User.findOne({ where: { id: hash_obj.id } });
 
         //update invitation accepted time
         let update_invitation = await Invitation.update(
           { accepted_on: new Date() },
           { where: { id: hash_obj.invitation_id } }
-        )
+        );
       } else {
         new_user = await User.create({
           ...value,
           password,
-        })
+        });
       }
       const token = generateToken({
         user: {
           id: new_user.id,
         },
-      })
+      });
       res.status(200).json({
         status: true,
         user: new_user,
         access_token: token,
-      })
+      });
     } catch (err) {
-      console.log(err.message)
+      console.log(err.message);
       res.status(500).json({
         status: false,
-        errors: 'Unable to save data',
-      })
+        errors: "Unable to save data",
+      });
     }
   }
 
   async login(req, res) {
     const schema = Joi.object({
       password: Joi.string()
-        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+        .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
         .required(),
       email: Joi.string().email().required(),
-    })
-    const { error, value } = schema.validate(req.body)
+    });
+    const { error, value } = schema.validate(req.body);
     if (error) {
-      let error_msg = error.details.map((err) => err.message)
+      let error_msg = error.details.map((err) => err.message);
       res.status(401).json({
         status: false,
-        errors: error_msg.join(','),
-      })
-      return
+        errors: error_msg.join(","),
+      });
+      return;
     }
-    const user = await User.findOne({ where: { email: value.email } })
+    const user = await User.findOne({ where: { email: value.email } });
     if (!user) {
       res.status(401).json({
         status: false,
-        errors: 'Email is not registered',
-      })
-      return
+        errors: "Email is not registered",
+      });
+      return;
     }
-    const isMatch = await bcrypt.compare(value.password, user.password)
+    const isMatch = await bcrypt.compare(value.password, user.password);
     if (!isMatch) {
       res.status(401).json({
         status: false,
-        errors: 'Invalid Credentials',
-      })
-      return
+        errors: "Invalid Credentials",
+      });
+      return;
     }
     const token = generateToken({
       user: {
         id: user.id,
       },
-    })
+    });
 
-    const companies = await user.getCompanies({ include: ['CompanyPortals'] })
-    
-    const userResourcesObj = new UserResources(user)
+    const companies = await user.getCompanies({ include: ["CompanyPortals"] });
+
+    const userResourcesObj = new UserResources(user);
     const userResourcesData = await userResourcesObj.getUserFormattedData();
 
     res.status(200).json({
@@ -180,37 +180,37 @@ class AuthController {
       user: userResourcesData,
       companies,
       access_token: token,
-    })
+    });
   }
 
   async profile(req, res) {
-    var groups = []
-    var roles = []
-    var permissions = []
-    let user = req.user
-    const header_company_id = req.header('company_id') || 0
+    var groups = [];
+    var roles = [];
+    var permissions = [];
+    let user = req.user;
+    const header_company_id = req.header("company_id") || 0;
 
-    const userResourcesObj = new UserResources(user,header_company_id)
+    const userResourcesObj = new UserResources(user, header_company_id);
     const userResourcesData = await userResourcesObj.getUserFormattedData();
-    
-    res.send({ status: true, user: userResourcesData })
+
+    res.send({ status: true, user: userResourcesData });
   }
 
   async logout(req, res) {
-    res.status(200).json({ status: true })
+    res.status(200).json({ status: true });
   }
 
   async refreshToken(req, res) {
-    const user = req.user
-    const companies = await user.getCompanies({ include: ['CompanyPortals'] })
+    const user = req.user;
+    const companies = await user.getCompanies({ include: ["CompanyPortals"] });
 
     const token = generateToken({
       user: {
         id: user.id,
       },
-    })
-    
-    const userResourcesObj = new UserResources(user)
+    });
+
+    const userResourcesObj = new UserResources(user);
     const userResourcesData = await userResourcesObj.getUserFormattedData();
 
     res.status(200).json({
@@ -218,7 +218,7 @@ class AuthController {
       access_token: token,
       user: userResourcesData,
       companies,
-    })
+    });
   }
 
   async getCompanyAndSites(req, res) {
@@ -227,119 +227,119 @@ class AuthController {
         id: req.user.id,
       },
       include: [{ all: true, nested: true }],
-    })
+    });
     res.status(200).json({
       status: true,
-      companies: 'Companies' in user && user.Companies ? user.Companies : [],
-    })
+      companies: "Companies" in user && user.Companies ? user.Companies : [],
+    });
   }
   //forgot password
   async forgotPassword(req, res) {
     const schema = Joi.object({
       email: Joi.string().email().required(),
-    })
-    const { error, value } = schema.validate(req.body)
+    });
+    const { error, value } = schema.validate(req.body);
     if (error) {
-      let error_msg = error.details.map((err) => err.message)
+      let error_msg = error.details.map((err) => err.message);
       res.status(401).json({
         status: false,
-        errors: error_msg.join(','),
-      })
-      return
+        errors: error_msg.join(","),
+      });
+      return;
     }
     //user details
-    const user = await User.findOne({ where: { email: value.email } })
+    const user = await User.findOne({ where: { email: value.email } });
     if (!user) {
       res.status(401).json({
         status: false,
-        errors: 'Email is not registered',
-      })
-      return
+        errors: "Email is not registered",
+      });
+      return;
     }
-    let reset_obj = { id: user.id, email: user.email }
-    reset_obj = JSON.stringify(reset_obj)
-    let base64data = Buffer.from(reset_obj, 'utf8')
-    let base64String = base64data.toString('base64')
+    let reset_obj = { id: user.id, email: user.email };
+    reset_obj = JSON.stringify(reset_obj);
+    let base64data = Buffer.from(reset_obj, "utf8");
+    let base64String = base64data.toString("base64");
     res.status(200).json({
       status: true,
       reset_link:
-        process.env.CLIENT_ORIGIN + '/reset-password?hash=' + base64String,
-      message: 'Reset password mail has been sent to your email',
-    })
+        process.env.CLIENT_ORIGIN + "/reset-password?hash=" + base64String,
+      message: "Reset password mail has been sent to your email",
+    });
   }
   //reset password
   async resetPassword(req, res) {
     const schema = Joi.object({
       hash: Joi.string().required(),
       password: Joi.string()
-        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+        .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
         .required(),
-    })
+    });
 
-    const { error, value } = schema.validate(req.body)
-    let hash_obj = Buffer.from(value.hash, 'base64')
-    hash_obj = hash_obj.toString('utf8')
-    hash_obj = JSON.parse(hash_obj)
+    const { error, value } = schema.validate(req.body);
+    let hash_obj = Buffer.from(value.hash, "base64");
+    hash_obj = hash_obj.toString("utf8");
+    hash_obj = JSON.parse(hash_obj);
 
     if (error) {
-      let error_msg = error.details.map((err) => err.message)
+      let error_msg = error.details.map((err) => err.message);
       res.status(401).json({
         status: false,
-        errors: error_msg.join(','),
-      })
-      return
+        errors: error_msg.join(","),
+      });
+      return;
     }
     //user details
-    const user = await User.findOne({ where: { id: hash_obj.id } })
+    const user = await User.findOne({ where: { id: hash_obj.id } });
     if (!user) {
       res.status(401).json({
         status: false,
-        errors: 'Email is not registered',
-      })
-      return
+        errors: "Email is not registered",
+      });
+      return;
     }
 
-    const salt = await bcrypt.genSalt(10)
-    const password = await bcrypt.hash(value.password, salt)
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(value.password, salt);
     let update_user = await User.update(
       { password: password },
       { where: { id: hash_obj.id } }
-    )
+    );
     res.status(200).json({
       status: true,
-      message: 'Password updated',
-    })
+      message: "Password updated",
+    });
   }
   //get user permissions
   async getUserPermissions(id) {
-    const user = await User.findByPk(id)
-    const companies = await user.getCompanies()
+    const user = await User.findByPk(id);
+    const companies = await user.getCompanies();
     //user roles
     let roles = await GroupRole.findAll({
-      attributes: ['group_id', 'role_id'],
+      attributes: ["group_id", "role_id"],
       where: { group_id: companies[0].company_user.group_id },
-    })
+    });
     roles = roles.map((role) => {
-      return role.role_id
-    })
+      return role.role_id;
+    });
     //user permissions based on the role
     let permissions = await Role.findAll({
       where: { id: roles },
       include: {
         model: Permission,
         required: true,
-        attributes: ['name', 'id', 'slug'],
+        attributes: ["name", "id", "slug"],
       },
-    })
+    });
     permissions = permissions.map((permission) => {
       return permission.Permissions.map((all_permission) => {
-        return all_permission.slug
-      })
-    })
+        return all_permission.slug;
+      });
+    });
     // permissions = [].concat.apply([], permissions)
-    permissions = permissions.flat()
-    return permissions
+    permissions = permissions.flat();
+    return permissions;
   }
 }
 
-module.exports = AuthController
+module.exports = AuthController;
