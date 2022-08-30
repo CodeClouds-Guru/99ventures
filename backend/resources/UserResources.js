eff057ebfe8bf21ba105aca0a05f04bc1574069d
CODECLOUDS-DEBOSMITA <codeclouds-debosmita@CISPL-IO-659.local>
2022-08-30 14:28:43 +0530

    commit for downtime api changes and userResource raw query modifications

--- backend/resources/UserResources.js
+++ backend/resources/UserResources.js
@@ -1,38 +1,32 @@
 // 'use strict'
 
-const { Role, Permission, Group } = require('../models/index')
-const { QueryTypes } = require('sequelize')
-const db = require('../models/index')
+const { Role, Permission, Group, CompanyUser } = require("../models/index");
+const { QueryTypes } = require("sequelize");
+const db = require("../models/index");
 
 module.exports = class UserResources {
   constructor(user, company_id = 0) {
-    this.user = user
-    this.company_id = company_id
+    this.user = user;
+    this.company_id = company_id;
   }
 
   async getUserFormattedData() {
     if (!this.user) {
-      return this.response
+      return this.response;
     }
-    var groups = []
-    var permissions = []
-    var company = []
-    var companies = []
-    var roles = []
-    let user = this.user
-
-    company = await user.getCompanies({ include: ['CompanyPortals'] })
+    var groups = [];
+    var permissions = [];
+    var company = [];
+    var companies = [];
+    var roles = [];
+    let user = this.user;
+    company = await user.getCompanies({ include: ["CompanyPortals"] });
 
     if (this.company_id !== 0) {
-      // var roles = [{ 'id': 1, 'name': 'Admin', 'slug': 'admin' }]
-      companies = await db.sequelize.query(
-        'SELECT * FROM company_user WHERE company_id = ? AND user_id = ? LIMIT 1',
-        {
-          replacements: [this.company_id, user.id],
-          type: QueryTypes.SELECT,
-        }
-      )
-      console.log('from user resource', this.company_id, user.id, companies)
+      companies = await CompanyUser.findAll({
+        where: { company_id: this.company_id, user_id: user.id },
+      });
+      console.log("from user resource", this.company_id, user.id, companies);
       if (companies && companies.length > 0) {
         const group = await Group.findOne({
           where: {
@@ -42,46 +36,40 @@ module.exports = class UserResources {
             {
               model: Role,
               through: {
-                attributes: ['group_id', 'role_id'],
+                attributes: ["group_id", "role_id"],
               },
               include: [
                 {
                   model: Permission,
                   through: {
-                    attributes: ['role_id', 'permission_id'],
+                    attributes: ["role_id", "permission_id"],
                   },
                 },
               ],
             },
           ],
-        })
-        groups = [group]
+        });
+        groups = [group];
         group.Roles.forEach((r) => {
           roles.push({
             id: r.id,
             name: r.name,
             slug: r.slug,
-          })
+          });
           r.Permissions.forEach((p) => {
-            permissions.push(p.slug)
-          })
-        })
-        permissions = [...new Set(permissions)]
+            permissions.push(p.slug);
+          });
+        });
+        permissions = [...new Set(permissions)];
       }
-      user.setDataValue('roles', roles)
+      user.setDataValue("roles", roles);
     } else {
-      company = await user.getCompanies({ include: ['CompanyPortals'] })
-      companies = await db.sequelize.query(
-        'SELECT * FROM company_user WHERE 1',
-        {
-          type: QueryTypes.SELECT,
-        }
-      )
+      companies = await CompanyUser.findAll();
     }
 
-    user.setDataValue('permissions', permissions)
-    user.setDataValue('groups', groups)
-    user.setDataValue('companies', company)
-    return user
+    user.setDataValue("permissions", permissions);
+    user.setDataValue("groups", groups);
+    user.setDataValue("companies", company);
+    return user;
   }
-}
+};
