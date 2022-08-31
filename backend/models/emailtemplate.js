7f23124db2e071e02f674d7522ba6bef7ddd5e27
Nandita <nandita.bardhan@codeclouds.co.in>
2022-08-31 16:35:42 +0530

    email template migration

---
+++ backend/models/emailtemplate.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class EmailTemplate extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  EmailTemplate.init({
+    subject: DataTypes.STRING,
+    body: DataTypes.TEXT,
+    company_portal_id: DataTypes.BIGINT,
+    created_by: DataTypes.BIGINT,
+    updated_by: DataTypes.BIGINT,
+    deleted_by: DataTypes.BIGINT,
+    created_at: 'TIMESTAMP',
+    updated_at: 'TIMESTAMP',
+    deleted_at: 'TIMESTAMP'
+  }, {
+    sequelize,
+    modelName: 'EmailTemplate',
+    timestamps: true,
+    paranoid: true,
+    createdAt: 'created_at', // alias createdAt as created_date
+    updatedAt: 'updated_at',
+    deletedAt: 'deleted_at',
+    tableName: 'email_templates',
+  });
+  return EmailTemplate;
+};
