637612f8cb7ce5c61bc3f0fa1946dbc487ef1dc6
CODECLOUDS-DEBOSMITA <codeclouds-debosmita@CISPL-IO-659.local>
2022-09-13 16:26:02 +0530

    commit for script apis

--- backend/controllers/backend/ScriptController.js
+++ backend/controllers/backend/ScriptController.js
@@ -1,8 +1,72 @@
 const Controller = require("./Controller");
+const { stringToSlug } = require("../../helpers/global");
+const { Script } = require("../../models/index");
 
 class ScriptController extends Controller {
   constructor() {
     super("Script");
+  }
+
+  async list(req, res) {
+    const options = this.getQueryOptions(req);
+    options.where = { company_portal_id: req.headers.site_id };
+
+    let page = req.query.page || 1;
+    let limit = parseInt(req.query.show) || 10; // per page record
+    let offset = (page - 1) * limit;
+    options.limit = limit;
+    options.offset = offset;
+    let result = await this.model.findAndCountAll(options);
+    let pages = Math.ceil(result.count / limit);
+    return {
+      result: { data: result.rows, pages, total: result.count },
+      fields: this.model.fields,
+    };
+  }
+
+  //override save function
+  async save(req, res) {
+    req.body.company_portal_id = req.headers.site_id;
+    try {
+      const script_name = req.body.name || "";
+      // req.body.code = stringToSlug(script_name) + '-'+new Date().getTime();
+      req.body.code =
+        script_name
+          .split(" ")
+          .reduce((response, word) => (response += word.slice(0, 1)), "") +
+        "-" +
+        new Date().getTime();
+      req.body.status = 0;
+      console.log("-----------------------", req.body);
+      let model = await super.save(req);
+      return {
+        message: "Record has been created successfully",
+        result: model,
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async update(req, res) {
+    let request_data = req.body;
+    let id = req.params.id;
+    try {
+      request_data.updated_by = req.user.id;
+      // let model = await this.model.create(request_data, { silent: true });
+      const result = Script.update(request_data, {
+        where: {
+          id: id,
+        },
+      });
+      // await super.save(request_data);
+      return {
+        message: "Record has been created successfully",
+        result: result,
+      };
+    } catch (error) {
+      throw error;
+    }
   }
 }
 module.exports = ScriptController;
--- backend/models/script.js
+++ backend/models/script.js
@@ -1,8 +1,7 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 const sequelizePaginate = require("sequelize-paginate");
+const Joi = require("joi");
 
 module.exports = (sequelize, DataTypes) => {
   class Script extends Model {
@@ -15,121 +14,135 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Script.init({
-    code:DataTypes.STRING,
-    company_portal_id: DataTypes.BIGINT,
-    name: DataTypes.STRING,
-    script_html: DataTypes.TEXT,
-    status: DataTypes.STRING,
-    created_by: DataTypes.BIGINT,
-    updated_by: DataTypes.BIGINT,
-    deleted_by: DataTypes.BIGINT,
-    created_at: 'TIMESTAMP',
-    updated_at: 'TIMESTAMP',
-    deleted_at: 'TIMESTAMP'
-  }, {
-    sequelize,
-    modelName: 'Script',
-    timestamps: true,
-    paranoid: true,
-    createdAt: 'created_at', // alias createdAt as created_date
-    updatedAt: 'updated_at',
-    deletedAt: 'deleted_at',
-    tableName: 'scripts',
-  });
+  Script.validate = function (req) {
+    const schema = Joi.object({
+      code: Joi.string().required().label("Code"),
+      company_portal_id: Joi.string().required().label("Company Portal Id"),
+      name: Joi.string().required().label("Name"),
+      script_html: Joi.string().required().label("Script HTML"),
+      status: Joi.number().optional(),
+    });
+    return schema.validate(req.body);
+  };
+
+  Script.init(
+    {
+      code: DataTypes.STRING,
+      company_portal_id: DataTypes.BIGINT,
+      name: DataTypes.STRING,
+      script_html: DataTypes.TEXT,
+      status: DataTypes.STRING,
+      created_by: DataTypes.BIGINT,
+      updated_by: DataTypes.BIGINT,
+      deleted_by: DataTypes.BIGINT,
+      created_at: "TIMESTAMP",
+      updated_at: "TIMESTAMP",
+      deleted_at: "TIMESTAMP",
+    },
+    {
+      sequelize,
+      modelName: "Script",
+      timestamps: true,
+      paranoid: true,
+      createdAt: "created_at", // alias createdAt as created_date
+      updatedAt: "updated_at",
+      deletedAt: "deleted_at",
+      tableName: "scripts",
+    }
+  );
   Script.fields = {
     id: {
-      field_name: 'id',
-      db_name: 'id',
-      type: 'text',
-      placeholder: 'Id',
+      field_name: "id",
+      db_name: "id",
+      type: "text",
+      placeholder: "Id",
       listing: false,
       show_in_form: false,
       sort: true,
       required: false,
-      value: '',
-      width: '50',
+      value: "",
+      width: "50",
       searchable: false,
     },
     code: {
-      field_name: 'code',
-      db_name: 'code',
-      type: 'text',
-      placeholder: 'Code',
+      field_name: "code",
+      db_name: "code",
+      type: "text",
+      placeholder: "Script ID",
       listing: true,
       show_in_form: false,
       sort: true,
       required: false,
-      value: '',
-      width: '50',
+      value: "",
+      width: "50",
       searchable: true,
     },
     company_portal_id: {
-      field_name: 'company_portal_id',
-      db_name: 'company_portal_id',
-      type: 'text',
-      placeholder: 'company_portal_id',
+      field_name: "company_portal_id",
+      db_name: "company_portal_id",
+      type: "text",
+      placeholder: "company_portal_id",
       listing: false,
       show_in_form: false,
       sort: true,
       required: true,
-      value: '',
-      width: '50',
+      value: "",
+      width: "50",
       searchable: true,
     },
     name: {
-      field_name: 'name',
-      db_name: 'name',
-      type: 'text',
-      placeholder: 'Name',
+      field_name: "name",
+      db_name: "name",
+      type: "text",
+      placeholder: "Name",
       listing: true,
       show_in_form: true,
       sort: true,
       required: true,
-      value: '',
-      width: '50',
+      value: "",
+      width: "50",
       searchable: true,
     },
     script_html: {
-      field_name: 'script_html',
-      db_name: 'script_html',
-      type: 'text',
-      placeholder: 'script_html',
+      field_name: "script_html",
+      db_name: "script_html",
+      type: "text",
+      placeholder: "Script",
       listing: true,
       show_in_form: true,
       sort: true,
       required: true,
-      value: '',
-      width: '50',
+      value: "",
+      width: "50",
       searchable: true,
     },
     status: {
-      field_name: 'status',
-      db_name: 'status',
-      type: 'text',
-      placeholder: 'Status',
+      field_name: "status",
+      db_name: "status",
+      type: "text",
+      placeholder: "Status",
       listing: true,
       show_in_form: true,
       sort: true,
       required: true,
-      value: '',
-      width: '50',
+      value: "",
+      width: "50",
       searchable: true,
     },
     created_at: {
-      field_name: 'created_at',
-      db_name: 'created_at',
-      type: 'text',
-      placeholder: 'Created at',
-      listing: true,
+      field_name: "created_at",
+      db_name: "created_at",
+      type: "text",
+      placeholder: "Created at",
+      listing: false,
       show_in_form: false,
       sort: true,
       required: true,
-      value: '',
-      width: '50',
+      value: "",
+      width: "50",
       searchable: false,
-    }
-  }
+    },
+  };
   sequelizePaginate.paginate(Script);
   return Script;
 };
