4cbe36eca763dc92a4a9f1a322a3eb8d2edc8b0c
Sourav <sourav.tapadar@codeclouds.in>
2022-09-19 18:18:20 +0530

    Permission UI

--- admin/src/app/main/crud/create-edit/components/PermissionSettings.js
+++ admin/src/app/main/crud/create-edit/components/PermissionSettings.js
@@ -15,7 +15,7 @@ function PermissionSettings(props) {
     const dispatch = useDispatch();
     const [expanded, setExpanded] = useState(false);
     const handleChange = (panel) => (event, isExpanded) => {
-        console.log(panel, isExpanded, event)
+        // console.log(panel, isExpanded, event)
         setExpanded(isExpanded ? panel : false);
     };
     const [actions, setActions] = useState([{
@@ -40,35 +40,72 @@ function PermissionSettings(props) {
                 setRolePermissions(rolPermissionsResp);
             })
     }
+    const handleParentChecbox = (type) => (event) => {
+        // event.preventDefault();
+        console.log(event, type);
+    }
+    const handleChildCheckbox = (type) => (event) => {
+        // event.preventDefault();
+        console.log(event, type);
+    }
     return (
         <div className="p-16">
-            <Typography variant="subtitle1" className="mb-5">Permissions</Typography>
+            <Typography variant="subtitle1" className="mb-5 pl-8">Permissions</Typography>
             <Paper sx={{ width: '100%', overflow: 'hidden' }}>
                 {
                     Object.keys(modules).map((module, key) => {
+                        // console.log(88, modules[module])
                         return (<Accordion key={`accordion-${module}-${key}`} expanded={expanded === module} onChange={handleChange(module)}>
                             <AccordionSummary
                                 expandIcon={<ExpandMoreIcon />}
                                 aria-controls={module + '-' + key}
                                 id={module + '-' + key}
                             >
-                                <Typography component={'div'} sx={{ width: '33%', flexShrink: 0 }}>
+                                <Typography component={'div'} sx={{ width: '33%', flexShrink: 0 }} className="pt-10 pl-5">
                                     {module}
                                 </Typography>
-                                <Typography component={'div'} sx={{ color: 'text.secondary' }}>
-                                    <FormGroup row>
-                                        <FormControlLabel id={`view-${module}-${key}`} control={<Checkbox />} label="View" />
-                                        {/* </FormGroup>
-                                    <FormGroup> */}
-                                        <FormControlLabel id={`update-${module}-${key}`} control={<Checkbox />} label="Update" />
+                                <Typography component={'div'} sx={{ color: 'text.secondary' }} className="flex justify-end px-10">
+                                    <FormGroup row={true}>
+                                        <FormControlLabel control={
+                                            <Checkbox onClick={handleParentChecbox('view')} id={`view-${module}-${key}`} />
+                                        } label="View" />
+                                        <FormControlLabel control={
+                                            <Checkbox onClick={handleParentChecbox('update')} id={`update-${module}-${key}`} />
+                                        } label="Update" />
+                                        <FormControlLabel control={
+                                            <Checkbox onClick={handleParentChecbox('delete')} id={`delete-${module}-${key}`} />
+                                        } label="Delete" />
                                     </FormGroup>
                                 </Typography>
                             </AccordionSummary>
                             <AccordionDetails>
-                                <Typography>
-                                    Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-                                    Aliquam eget maximus est, id dignissim quam.
-                                </Typography>
+                                {
+                                    modules[module].map((childModule, childKey) => {
+                                        // console.log(childModule.name);
+                                        // return;
+                                        return (
+                                            <>
+                                                <Typography component={'div'} row={true} className="flex justify-between px-20 py-5">
+                                                    <span className="pt-10">
+                                                        {childModule.name}
+                                                    </span>
+                                                    <FormGroup row={true} className="">
+                                                        <FormControlLabel control={
+                                                            <Checkbox onClick={handleChildCheckbox('view')} id={`view-${childModule.slug}-${childKey}`} />
+                                                        } label="View" />
+                                                        <FormControlLabel control={
+                                                            <Checkbox onClick={handleChildCheckbox('update')} id={`update-${childModule.slug}-${childKey}`} />
+                                                        } label="Update" />
+                                                        <FormControlLabel control={
+                                                            <Checkbox onClick={handleChildCheckbox('delete')} id={`delete-${childModule.slug}-${childKey}`} />
+                                                        } label="Delete" />
+                                                    </FormGroup>
+                                                </Typography>
+                                            </>
+                                        )
+
+                                    })
+                                }
                             </AccordionDetails>
                         </Accordion>)
                     })
