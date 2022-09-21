1dbe1a2d32412374f29d21e4deea812261ec5fb6
Sourav <sourav.tapadar@codeclouds.in>
2022-09-21 19:32:01 +0530

    Permission revamp

--- admin/src/app/main/crud/create-edit/components/PermissionSettings.js
+++ admin/src/app/main/crud/create-edit/components/PermissionSettings.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Typography, Paper, TableContainer, Table, TableBody, TableHead, TableRow, TableCell, Button, Accordion, AccordionDetails, AccordionSummary, FormGroup, FormControlLabel, Checkbox } from '@mui/material';
+import { Typography, Paper, Button, Accordion, AccordionDetails, AccordionSummary, FormGroup, FormControlLabel, Checkbox } from '@mui/material';
 import axios from "axios";
 import { motion } from 'framer-motion';
 import { showMessage } from 'app/store/fuse/messageSlice';
@@ -10,107 +10,173 @@ import { setUser } from 'app/store/userSlice';
 import { resetNavigation } from 'app/store/fuse/navigationSlice';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import jwtServiceConfig from 'src/app/auth/services/jwtService/jwtServiceConfig';
+import { indexOf } from 'lodash';
 
 function PermissionSettings(props) {
     const dispatch = useDispatch();
-    const [expanded, setExpanded] = useState(false);
-    const handleChange = (panel) => (event, isExpanded) => {
+    const [expanded, setExpanded] = useState(true);
+    const handleAccordionClick = (panel) => (event, isExpanded) => {
         // console.log(panel, isExpanded, event)
         setExpanded(isExpanded ? panel : false);
     };
-    const [actions, setActions] = useState([{
-        slug: 'module',
-        name: 'Module',
-        minWidth: 170,
-        align: 'left'
-    },]);
-    const [modules, setModules] = useState([]);
-    const [rolePermissions, setRolePermissions] = useState([]);
+    // const [actions, setActions] = useState([{
+    //     slug: 'module',
+    //     name: 'Module',
+    //     minWidth: 170,
+    //     align: 'left'
+    // },]);
+    const [modules, setModules] = useState({});
+    // const [rolePermissions, setRolePermissions] = useState([]);
     const [openAlertDialog, setOpenAlertDialog] = useState(false);
+    // const [selectedPermissions, setSelectedPermissions] = useState({});
     useEffect(() => {
         getRolePermissions();
     }, [])
     const getRolePermissions = () => {
         axios.get(jwtServiceConfig.roleEdit + `/${props.roleId}`)
             .then(res => {
-                console.log(res)
                 // setActions((prevState) => [...prevState, ...res.data.results.actions]);
                 setModules(res.data.results.new_modules);
-                let rolPermissionsResp = res.data.results.role_permissions.map(rolePermission => rolePermission.slug);
-                setRolePermissions(rolPermissionsResp);
+                // let rolPermissionsResp = res.data.results.role_permissions.map(rolePermission => rolePermission.slug);
+                // setRolePermissions(rolPermissionsResp);
+
+                /* set parent module in structure */
+                // Object.keys(res.data.results.new_modules).map((module, key) => {
+                // res.data.results.new_modules[module].map((childModule, childKey) => {
+                //     childModule.insertedAction = [];
+                // })
+                //     setSelectedPermissions(
+                //         selectedPermissions => ({
+                //             ...selectedPermissions, ...{ [module]: [] }
+                //         })
+                //     )
+                // })
             })
     }
     const handleParentChecbox = (type) => (event) => {
         // event.preventDefault();
         console.log(event, type);
     }
-    const handleChildCheckbox = (type) => (event) => {
+    const handleChildCheckbox = (type, childModule) => (event) => {
         // event.preventDefault();
-        console.log(event, type);
+        Object.keys(modules).map((module, key) => {
+            modules[module].map((child_module, child_key) => {
+                if (child_module.slug === childModule.slug) {
+                    if (event.target.checked) {
+                        child_module.action.push(type);
+                    } else {
+                        child_module.action.splice(child_module.action.indexOf(type), 1)
+                    }
+                }
+            })
+        })
+        setModules(modules => ({
+            ...modules, ...modules
+        }))
+    }
+    const onConfirmAlertDialogHandle = () => {
+        console.log({ requestType: 'apply-permission', role_permissions: modules });
+        return
+        axios.post(`${jwtServiceConfig.roleUpdate}/${props.roleId}`, { requestType: 'apply-permission', role_permissions: modules })
+            .then(res => {
+                dispatch(showMessage({ variant: 'success', message: 'Permissions applied successfully.' }));
+                jwtService.getProfile()
+                    .then(user => {
+                        dispatch(resetNavigation());
+                        dispatch(setUser(user));
+                    })
+                setOpenAlertDialog(false);
+                getRolePermissions();
+            }).catch(err => {
+                dispatch(showMessage({ variant: 'error', message: 'Something went wrong!' }));
+                setOpenAlertDialog(false);
+            })
     }
     return (
         <div className="p-16">
+            <AlertDialog
+                open={openAlertDialog}
+                onConfirm={onConfirmAlertDialogHandle}
+                onClose={() => { setOpenAlertDialog(false); }}
+            />
             <Typography variant="subtitle1" className="mb-5 pl-8">Permissions</Typography>
             <Paper sx={{ width: '100%', overflow: 'hidden' }}>
                 {
                     Object.keys(modules).map((module, key) => {
-                        // console.log(88, modules[module])
-                        return (<Accordion key={`accordion-${module}-${key}`} expanded={expanded === module} onChange={handleChange(module)}>
-                            <AccordionSummary
-                                expandIcon={<ExpandMoreIcon />}
-                                aria-controls={module + '-' + key}
-                                id={module + '-' + key}
-                            >
-                                <Typography component={'div'} sx={{ width: '33%', flexShrink: 0 }} className="pt-10 pl-5">
-                                    {module}
-                                </Typography>
-                                <Typography component={'div'} sx={{ color: 'text.secondary' }} className="flex justify-end px-10">
-                                    <FormGroup row={true}>
-                                        <FormControlLabel control={
-                                            <Checkbox onClick={handleParentChecbox('view')} id={`view-${module}-${key}`} />
-                                        } label="View" />
-                                        <FormControlLabel control={
-                                            <Checkbox onClick={handleParentChecbox('update')} id={`update-${module}-${key}`} />
-                                        } label="Update" />
-                                        <FormControlLabel control={
-                                            <Checkbox onClick={handleParentChecbox('delete')} id={`delete-${module}-${key}`} />
-                                        } label="Delete" />
-                                    </FormGroup>
-                                </Typography>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                {
-                                    modules[module].map((childModule, childKey) => {
-                                        // console.log(childModule.name);
-                                        // return;
-                                        return (
-                                            <>
-                                                <Typography component={'div'} row={true} className="flex justify-between px-20 py-5">
+                        return (
+                            <Accordion key={`accordion-${module}-${key}`} expanded={expanded === module} onChange={handleAccordionClick(module)}>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls={module + '-' + key}
+                                    id={module + '-' + key}
+                                >
+                                    <Typography component={'div'} sx={{ width: '33%', flexShrink: 0 }} className="pt-10 pl-5">
+                                        {module}
+                                    </Typography>
+                                    <Typography component={'div'} sx={{ color: 'text.secondary' }} className="flex justify-end px-10">
+                                        <FormGroup row={true}>
+                                            <FormControlLabel control={
+                                                <Checkbox onClick={handleParentChecbox('view')} id={`view-${module}-${key}`} />
+                                            } label="View" />
+                                            <FormControlLabel control={
+                                                <Checkbox onClick={handleParentChecbox('update')} id={`update-${module}-${key}`} />
+                                            } label="Update" />
+                                            <FormControlLabel control={
+                                                <Checkbox onClick={handleParentChecbox('delete')} id={`delete-${module}-${key}`} />
+                                            } label="Delete" />
+                                        </FormGroup>
+                                    </Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    {
+                                        modules[module].map((childModule, childKey) => {
+                                            // console.log(childModule.name);
+                                            // return;
+                                            return (
+                                                <Typography key={`accordion-${childModule}-${childKey}`} component={'div'} row={true} className="flex justify-between px-20 py-5">
                                                     <span className="pt-10">
                                                         {childModule.name}
                                                     </span>
                                                     <FormGroup row={true} className="">
                                                         <FormControlLabel control={
-                                                            <Checkbox onClick={handleChildCheckbox('view')} id={`view-${childModule.slug}-${childKey}`} />
+                                                            <Checkbox onClick={handleChildCheckbox('view', childModule)} id={`view-${childModule.slug}-${childKey}`}
+                                                                checked={childModule.action.includes('view')}
+                                                            />
                                                         } label="View" />
                                                         <FormControlLabel control={
-                                                            <Checkbox onClick={handleChildCheckbox('update')} id={`update-${childModule.slug}-${childKey}`} />
+                                                            <Checkbox onClick={handleChildCheckbox('update', childModule)} id={`update-${childModule.slug}-${childKey}`}
+                                                                checked={childModule.action.includes('update')}
+                                                            />
                                                         } label="Update" />
                                                         <FormControlLabel control={
-                                                            <Checkbox onClick={handleChildCheckbox('delete')} id={`delete-${childModule.slug}-${childKey}`} />
+                                                            <Checkbox onClick={handleChildCheckbox('delete', childModule)} id={`delete-${childModule.slug}-${childKey}`}
+                                                                checked={childModule.action.includes('delete')}
+                                                            />
                                                         } label="Delete" />
                                                     </FormGroup>
                                                 </Typography>
-                                            </>
-                                        )
-
-                                    })
-                                }
-                            </AccordionDetails>
-                        </Accordion>)
+                                            )
+                                        })
+                                    }
+                                </AccordionDetails>
+                            </Accordion>)
                     })
                 }
             </Paper>
+            <motion.div
+                className="flex mt-16"
+                initial={{ opacity: 0, x: 20 }}
+                animate={{ opacity: 1, x: 0, transition: { delay: 0.3 } }}
+            >
+                <Button
+                    className="whitespace-nowrap mx-4 mt-5"
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => { setOpenAlertDialog(true); }}
+                >
+                    Apply Permissions to Role
+                </Button>
+            </motion.div>
         </div>
     )
 
