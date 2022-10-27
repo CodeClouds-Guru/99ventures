a97e497390b8c4952f05f74ee627db882e739d29
CODECLOUDS-DEBOSMITA <codeclouds-debosmita@CISPL-IO-659.local>
2022-10-27 18:39:50 +0530

    commit for api modification for settings

--- backend/seeders/20220922090339-add-settings.js
+++ backend/seeders/20220922090339-add-settings.js
@@ -3,10 +3,11 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     // Add seed commands here.
-    var obj = {
+    var fileManagerConfigObj = {
       images: ["jpeg", "jpg", "png", "gif"],
       files: ["css", "js", "html"],
       documents: ["xlsx", "pdf", "xlx", "docx", "doc", "odt"],
+      audio: ["mp3"],
     };
     await queryInterface.bulkInsert(
       "settings",
@@ -14,8 +15,24 @@ module.exports = {
         {
           id: 1,
           company_portal_id: 1,
-          settings_key: "file_config",
-          settings_value: JSON.stringify(obj),
+          settings_key: "file_manager_configuration",
+          settings_value: JSON.stringify(fileManagerConfigObj),
+          created_by: 1,
+          created_at: new Date(),
+        },
+        {
+          id: 2,
+          company_portal_id: 1,
+          settings_key: "max_file_size",
+          settings_value: null,
+          created_by: 1,
+          created_at: new Date(),
+        },
+        {
+          id: 3,
+          company_portal_id: 1,
+          settings_key: "max_no_of_uploads",
+          settings_value: null,
           created_by: 1,
           created_at: new Date(),
         },
