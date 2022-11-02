ef30368d04a1666f5f93c888db15147d67976e1b
Nandita <nandita.bardhan@codeclouds.co.in>
2022-11-02 16:57:59 +0530

    layout migration

--- backend/migrations/20221102122216-modify-layout.js
+++ backend/migrations/20221102122216-modify-layout.js
@@ -3,7 +3,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.addColumn("layouts", "html", {
-      type: Sequelize.JSON
+      type: Sequelize.TEXT('long')
     });
   },
 
