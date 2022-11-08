a47d1b4054ec1a065124c632179129ece316c248
Soumen Kamila <soumen.kamila@codeclouds.in>
2022-11-08 19:14:44 +0530

    hide preview btn

--- admin/src/app/main/layouts/CreateEditForm.js
+++ admin/src/app/main/layouts/CreateEditForm.js
@@ -262,14 +262,14 @@ const CreateEditForm = () => {
                     initial={{ opacity: 0, x: 20 }}
                     animate={{ opacity: 1, x: 0, transition: { delay: 0.3 } }}
                 >
-                    <Button
+                    {/* <Button
                         className="whitespace-nowrap mx-4"
                         variant="contained"
                         color="primary"
                         disabled={ true }
                     >
                         Preview
-                    </Button>
+                    </Button> */}
                     {
                         (hasPermission('save') || hasPermission('update')) && (
                             <LoadingButton
