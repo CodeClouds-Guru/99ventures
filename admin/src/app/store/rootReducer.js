4094efbad6a5cbb7bb988ed94dbd6e8c6a014e69
Soumen Kamila <soumen.kamila@codeclouds.in>
2022-11-14 16:22:18 +0530

    Layout revision implemented

--- admin/src/app/store/rootReducer.js
+++ admin/src/app/store/rootReducer.js
@@ -4,6 +4,7 @@ import i18n from './i18nSlice';
 import user from './userSlice';
 import account from './accountSlice'
 import filemanager from './filemanager';
+import layout from './layout';
 
 const createReducer = (asyncReducers) => (state, action) => {
   const combinedReducer = combineReducers({
@@ -12,6 +13,7 @@ const createReducer = (asyncReducers) => (state, action) => {
     user,
     account,
     filemanager,
+    layout,
     ...asyncReducers,
   });
 
