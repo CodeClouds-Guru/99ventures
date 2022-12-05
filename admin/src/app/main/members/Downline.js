6782bdc859507b62165637ab338b70ea60e09202
Soumen Kamila <soumen.kamila@codeclouds.in>
2022-12-05 16:17:01 +0530

    downline history api integration

--- admin/src/app/main/members/Downline.js
+++ admin/src/app/main/members/Downline.js
@@ -12,9 +12,10 @@ import {
     TimelineDot
 } from '@mui/lab';
 import ShareIcon from '@mui/icons-material/Share';
-import CommonHeader from './CommonHeader';
 import axios from 'axios';
 import PageHeader from './PageHeader';
+import Helper from 'src/app/helper';
+import { Link } from 'react-router-dom';
 
 const Downline = () => {
     const isMobile = useThemeMediaQuery((theme) => theme.breakpoints.down('lg'));
@@ -25,11 +26,14 @@ const Downline = () => {
         const params = {
             where: {
                 member_id: 1
-            }
+            },
+            sort: 'activity_date',
+            sort_order: 'desc'
         }
         axios.get('/member-referrals', { params })
         .then(res => {
             if(res.data.results.result.data) {
+                console.log(res.data.results.result.data)
                 setDownlineHistory(res.data.results.result.data);
             }
         })
@@ -43,6 +47,9 @@ const Downline = () => {
         getDownlineHistory();
     }, []);
     
+    const memberAvtar = (member) => {
+        return member.avatar ? member.avatar : `https://ui-avatars.com/api/?name=${ member.first_name}+${ member.last_name}`
+    }
 
     return (
         <FusePageCarded
@@ -53,102 +60,63 @@ const Downline = () => {
             content={
                 <Box className="sm:p-16 lg:p-22 md:p-16 xl:p-32 flex " >
                     <Timeline >
-                        <TimelineItem >
-                            <TimelineOppositeContent
-                                sx={{ m: 'auto 0' }}
-                                align="right"
-                                variant="body2"
-                                color="text.secondary"
-                                className="flex-none w-192"
-                            >
-                                December 2022
-                            </TimelineOppositeContent>
-                            <TimelineSeparator>
-                                <TimelineConnector sx={{ backgroundColor: '#fff'}} />                     
-                                <TimelineDot color="primary">
-                                    <ShareIcon />
-                                </TimelineDot>
-                                <TimelineConnector />
-                            </TimelineSeparator>
-                            <TimelineContent sx={{ py: '12px', px: 2 }}>
-                                <div className="bg-gray-200 p-16 rounded-md w-1/2">
-                                    <div className='flex items-center mb-7'>
-                                        <Avatar alt="Remy Sharp" src="/material-ui-static/images/avatar/1.jpg" className="mr-7" />
-                                        <Typography variant="body1 font-bold" component="span">
-                                            John Smith
-                                        </Typography>
-                                    </div>
-                                    <Typography variant="caption" component="p">
-                                        2022-12-01 9:30 am
-                                    </Typography>
-                                    <Divider className="my-5" />
-                                    <Typography>Because you need strength</Typography>
-                                </div>
-                            </TimelineContent>
-                        </TimelineItem>
-                        <TimelineItem >
-                            <TimelineOppositeContent
-                                sx={{ m: 'auto 0' }}
-                                align="right"
-                                variant="body2"
-                                color="text.secondary"
-                                className="flex-none w-192"
-                            >
-                                November 2022
-                            </TimelineOppositeContent>
-                            <TimelineSeparator>
-                                <TimelineConnector />
-                                <TimelineDot color="primary">
-                                    <ShareIcon />
-                                </TimelineDot>
-                                <TimelineConnector />
-                            </TimelineSeparator>
-                            <TimelineContent sx={{ py: '12px', px: 2 }}>
-                                <div className="bg-gray-200 p-16 rounded-md w-1/2">
-                                    <div className='flex items-center mb-7'>
-                                        <Avatar alt="Remy Sharp" src="/material-ui-static/images/avatar/1.jpg" className="mr-7" />
-                                        <Typography variant="body1 font-bold" component="span">
-                                            John Smith
-                                        </Typography>
-                                    </div>
-                                    <Typography variant="caption" component="p">
-                                        2022-12-01 9:30 am
-                                    </Typography>
-                                    <Divider className="my-5" />
-                                    <Typography>Because you need strength</Typography>
-                                </div>
-                            </TimelineContent>
-                        </TimelineItem>
-                        <TimelineItem >
-                            <TimelineOppositeContent
-                                sx={{ m: 'auto 0' }}
-                                align="right"
-                                className="flex-none w-192"
-                            >
-                            </TimelineOppositeContent>                     
-                            <TimelineSeparator>
-                                <TimelineConnector />
-                                <TimelineDot color="primary">
-                                    <ShareIcon />
-                                </TimelineDot>
-                                <TimelineConnector sx={{ backgroundColor: '#fff'}} />
-                            </TimelineSeparator>                            
-                            <TimelineContent sx={{ py: '12px', px: 2 }}>
-                                <div className="bg-gray-200 p-16 rounded-md w-1/2">
-                                    <div className='flex items-center mb-7'>
-                                        <Avatar alt="Remy Sharp" src="/material-ui-static/images/avatar/1.jpg" className="mr-7" />
-                                        <Typography variant="body1 font-bold" component="span">
-                                            John Smith
-                                        </Typography>
-                                    </div>
-                                    <Typography variant="caption" component="p">
-                                        2022-12-01 9:30 am
-                                    </Typography>
-                                    <Divider className="my-5" />
-                                    <Typography>Because you need strength</Typography>
-                                </div>
-                            </TimelineContent>
-                        </TimelineItem>
+                        {
+                            downlineHistory.map((history, indx) => {
+                                return history.data.map((item, itemIndx) => {
+                                    return (
+                                        <TimelineItem key={indx}>                                            
+                                            <TimelineOppositeContent
+                                                sx={{ m: 'auto 0' }}
+                                                align="right"
+                                                variant="body2"
+                                                color="text.secondary"
+                                                className="flex-none w-192"
+                                            >
+                                                { itemIndx < 1 && history.date_group }
+                                            </TimelineOppositeContent>                                            
+                                                
+                                            <TimelineSeparator>
+                                                <TimelineConnector sx={ (indx < 1 && itemIndx < 1) && { backgroundColor: '#fff'}} />                     
+                                                <TimelineDot color="primary">
+                                                    <ShareIcon />
+                                                </TimelineDot>
+                                                <TimelineConnector sx={ (downlineHistory.length < 2 || (history.data.length-1 === itemIndx && indx !=0)) ? { backgroundColor: '#fff'} : {}} />
+                                            </TimelineSeparator>
+                                            <TimelineContent sx={{ py: '12px', px: 2 }}>
+                                                <div className="bg-gray-200 p-16 rounded-md w-1/2">
+                                                    <div className='flex items-center mb-7'>                                                        
+                                                        {
+                                                            item.Member && (
+                                                                <>
+                                                                    <Avatar 
+                                                                        alt="Remy Sharp" 
+                                                                        src={ memberAvtar(item.Member) } 
+                                                                        className="mr-7" 
+                                                                    />
+                                                                    <Link to={ `/app/members/${item.Member.id}`} style={{ textDecoration: 'none', color: '#1e293b'}}>
+                                                                        <Typography variant="body1 font-bold" component="span">
+                                                                            { item.Member.first_name +' '+ item.Member.last_name }
+                                                                        </Typography>
+                                                                    </Link>
+                                                                </>
+                                                            )
+                                                        }        
+                                                        <Typography variant="caption" component="p" className="ml-auto">
+                                                            {Helper.parseTimeStamp(item.created_at)}
+                                                        </Typography>                                                
+                                                    </div>
+                                                    
+                                                    <Divider className="my-5" />
+                                                    <Typography variant="caption">Status: </Typography>
+                                                </div>
+                                            </TimelineContent>
+                                        </TimelineItem>
+    
+                                    )
+                                })                                
+                            })
+                        }
+                       
                     </Timeline>
                 </Box>
             }
