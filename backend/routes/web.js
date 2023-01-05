b22da277b1750d80d8c8ecc31aaa1da28839ac95
cc-sourabh <sourabh.das@codeclouds.in>
2023-01-05 19:23:46 +0530

    sluggify

--- backend/routes/web.js
+++ backend/routes/web.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const PageParser = require('../helpers/PageParser')
-router.get('/', async (req, res) => {
-  var pagePerser = new PageParser('/');
+router.get('/:slug', async (req, res) => {
+  var pagePerser = new PageParser(req.params.slug || '/');
   try {
     var page_content = await pagePerser.preview();
   } catch (e) {
@@ -20,38 +20,6 @@ router.get('/', async (req, res) => {
   res.render('page', { page_content });
 });
 
-router.get('/preview/:slug?', async (req, res) => {
-  console.log(req.params.slug || '/');
-  var pagePerser = new PageParser(req.params.slug || '/');
-  try {
-    var page_content = await pagePerser.preview();
-  } catch (e) {
-    switch (e.statusCode) {
-      case 404:
-        res.redirect('/preview/404');
-        return;
-      default:
-        res.redirect('/preview/500');
-        console.error(e)
-        return;
-    }
-    page_content = await pagePerser.preview();
-  }
-  res.render('page', { page_content });
-});
-
-router.get('/test-crypt', async (req, res) => {
-  const Crypt = require("../helpers/Crypt");
-  const obj = {
-    email: 'sourabh@mailinator.com',
-    id: 1,
-    company_portal_id: 1,
-    company_id: 1
-  }
-  let cryptic = new Crypt(JSON.stringify(obj));
-  let str = cryptic.encrypt();
-  res.send(str);
-});
 module.exports = {
   prefix: '/',
   router,
