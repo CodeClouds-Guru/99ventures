618fee5e53c14048a613d605192619d552260404
Sourav <sourav.tapadar@codeclouds.in>
2023-01-05 16:22:39 +0530

    Avatar upload loader

---
+++ admin/src/app/shared-components/customLoader/Index.js
@@ -0,0 +1,12 @@
+import './Style.css';
+
+const Index = () => {
+    return (<div class="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>)
+}
+
+export default Index;
