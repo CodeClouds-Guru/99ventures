340b0f04945769d69a17fd731207fde6cecba918
Nandita <nandita.bardhan@codeclouds.co.in>
2023-01-10 14:04:42 +0530

    offer wall controller

---
+++ backend/controllers/backend/OfferWallController.js
@@ -0,0 +1,5 @@
+const Controller = require("./Controller");
+const { Op } = require("sequelize");
+const { OfferWall } = require("../../models/index");
+class OfferWallController extends Controller {
+}
