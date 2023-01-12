b80cfbe1cccd991770e80e49a50b2dfd887655fb
Soumen Kamila <soumen.kamila@codeclouds.in>
2023-01-12 11:54:33 +0530

    added offerwalls edit link

--- admin/src/app/main/crud/list/List.js
+++ admin/src/app/main/crud/list/List.js
@@ -220,6 +220,8 @@ function List(props) {
 		if (module === 'users' && item.id == user.id) {
 			dispatch(showMessage({ variant: 'warning', message: 'You are not allowed to edit this user' }));
 			return '';
+		} else if(module === 'offer-walls' || module === 'offerwalls'){
+			props.navigate(`/app/campaigns/${item.campaign_id}/offerwalls/${item.id}`);
 		} else {
 			props.navigate(`/app/${module}/${item.id}`);
 		}
