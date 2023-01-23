6479da132a8664006358d69de3716ad4b9e888c3
cc-sourabh <sourabh.das@codeclouds.in>
2023-01-23 21:00:20 +0530

    Logger

--- backend/routes/callback.js
+++ backend/routes/callback.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const logger = require("../helpers/Logger")();
 
 router.get('/test-adgate', (req, res) => {
-  console.log(req.query);
+  console.dir(logger);
+  logger.info(
+    JSON.stringify(req.query)
+  );
   res.send(req.query);
 });
 module.exports = {
