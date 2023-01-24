15687621549fe88d22f2075cc658f0773436fa2f
Nandita <nandita.bardhan@codeclouds.co.in>
2023-01-24 15:18:49 +0530

    socket connection

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,12 @@ const app = base()
 global.appRoot = path.resolve(__dirname);
 const server = app.listen(process.env.PORT || 4000)
 var io = require('socket.io')(server);
+global.io = io; //declared globaly
+
 io.sockets.on('connection', (socket) => {
+    global.socket = socket; //declared globaly
     console.log('user connected');
-    // socket.on('shoutbox', (newMessage) => {
+    // socket.on('news', (newMessage) => {
     //     console.log('newMessage', newMessage);
     // });
     socket.emit("shoutbox", { name: 'Tez', place: 'USA', message: 'Survey credited' });
