706bbb138c46edaa44c389dee807e31b7aaa6d6c
CODECLOUDS-DEBOSMITA <codeclouds-debosmita@CISPL-IO-659.local>
2023-02-02 15:44:33 +0530

    commit for member profile issue fixing

--- backend/controllers/backend/MemberController.js
+++ backend/controllers/backend/MemberController.js
@@ -224,10 +224,7 @@ class MemberController extends Controller {
         });
         console.log('survey_list', survey_list);
         for (let i = 0; i < survey_list.length; i++) {
-          if (
-            survey_list[i].Surveys &&
-            'SurveyProvider' in survey_list[i].Surveys[0]
-          )
+          if (survey_list[i].Surveys && survey_list[i].Surveys.length() > 0)
             survey_list[i].setDataValue(
               'name',
               survey_list[i].Surveys[0].SurveyProvider.name
