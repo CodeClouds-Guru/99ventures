5a45765c7e16ab069b484a445154840520b9076a
Sourav <sourav.tapadar@codeclouds.in>
2023-02-16 20:21:09 +0530

    Separate offerwall

--- admin/src/app/main/members/campaigns/offerwalls/Index.js
+++ admin/src/app/main/members/campaigns/offerwalls/Index.js
@@ -1,10 +1,6 @@
-import FusePageCarded from '@fuse/core/FusePageCarded';
 import useThemeMediaQuery from '@fuse/hooks/useThemeMediaQuery';
-import { Box } from '@mui/material';
-import MainHeader from 'app/shared-components/MainHeader';
 import List from '../../../crud/list/List';
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
 
 const Index = () => {
     const isMobile = useThemeMediaQuery((theme) => theme.breakpoints.down('lg'));
@@ -15,11 +11,6 @@ const Index = () => {
         <List
             params={(moduleId && !isNaN(moduleId)) ? { report: 1, campaign_id: moduleId } : {}}
             module={module}
-            customAddURL={`/app/campaigns/${moduleId}/offerwalls/create`}
-            where=""
-            addable={(moduleId && !isNaN(moduleId)) ? true : false}
-            editable={true}
-            deletable={(moduleId && !isNaN(moduleId)) ? true : false}
         />
     )
 }
