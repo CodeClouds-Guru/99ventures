0ecce8c737d9f0c67558ef66959f76ec7afb61e3
Nandita <nandita.bardhan@codeclouds.co.in>
2023-02-20 18:58:09 +0530

    survey sync

--- backend/controllers/callback/SurveycallbackController.js
+++ backend/controllers/callback/SurveycallbackController.js
@@ -53,36 +53,59 @@ class SurveycallbackController {
   }
 
   async syncSurvey(req, res) {
-    const logger1 = require('../../helpers/Logger')(
-      req.params.provider + '.log'
-    );
-    //   console.log('===================req', req);
-    logger1.info(JSON.stringify(req.query));
-    logger1.info(JSON.stringify(req.body));
+    // const logger1 = require('../../helpers/Logger')(
+    //   req.params.provider + '.log'
+    // );
+    // //   console.log('===================req', req);
+    // logger1.info(JSON.stringify(req.query));
+    // logger1.info(JSON.stringify(req.body));
     let survey = req.body;
     // let survey = require('./test.json');
     // console.log(survey, survey.length);
 
     if (survey.length > 0) {
       const provider = req.params.provider;
+      //get survey provider
       let survey_provider = await SurveyProvider.findOne({
         attributes: ['id'],
         where: { name: provider.charAt(0).toUpperCase() + provider.slice(1) },
       });
+      //survey questions
       let survey_questions = await SurveyQuestion.findAll({
         attributes: ['survey_provider_question_id'],
       });
       survey.map(async (record) => {
-        let model = await Survey.create(
-          {
-            survey_provider_id: survey_provider.id,
-            loi: record.length_of_interview,
-            cpi: record.cpi,
-            name: record.survey_name,
-            survey_number: record.survey_id,
-          },
-          { silent: true }
-        );
+        let status = 'draft'
+        if(record.is_live == true || record.is_live == 'true'){
+          status = 'active'
+        }
+        //create survey
+        let model = {};
+        // if(record.message_reason == 'updated'){
+        //    model = await Survey.update(
+        //     {
+        //       survey_provider_id: survey_provider.id,
+        //       loi: record.length_of_interview,
+        //       cpi: record.cpi,
+        //       name: record.survey_name,
+        //       survey_number: record.survey_id,
+        //     },
+        //     { where: { survey_number: record.survey_id,} }
+        //   );
+        // }else{
+          model = await Survey.create(
+            {
+              survey_provider_id: survey_provider.id,
+              loi: record.length_of_interview,
+              cpi: record.cpi,
+              name: record.survey_name,
+              survey_number: record.survey_id,
+              status:status,
+              original_json:record
+            },
+            { silent: true }
+          );
+        // }
         if (record.survey_qualifications.length > 0) {
           record.survey_qualifications.map(async (record1) => {
             let obj = survey_questions.find(
@@ -117,6 +140,16 @@ class SurveycallbackController {
           });
         }
       });
+
+      res.json({
+        status:true,
+        message:"Survey Updated."
+      })
+    }else{
+      res.json({
+        status:false,
+        message:"No survey found."
+      })
     }
   }
 }
