f3f79377b812821888f6361f60a0412fac83ce5d
Sourav <sourav.tapadar@codeclouds.in>
2023-02-23 15:32:28 +0530

    Fixes

--- backend/public/js/FormDataHandler.js
+++ backend/public/js/FormDataHandler.js
@@ -1,8 +1,37 @@
 $(() => {
-    var validation = () => {
-        $("input[name=email]").val().trim().length === 0 ? console.log('Email required') : '';
-        $("input[name=password]").val().trim().length === 0 ? console.log('Password required') : '';
-        if ($("input[name=email]").val().trim().length > 0 && $("input[name=password]").val().trim().length > 0) {
+    var loginErrors = [];
+    var loginErrorMsgs = [];
+    var dynamicErrorMsg = () => {
+        // loginErrors.includes('email') ? loginErrorMsgs.push('Valid')
+    }
+    var validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    }
+    var loginFormSanitisation = () => {
+        const form = $("#scripteed_login_form");
+        if (form.length === 1 && ["email", "password", "remember_me", "login_button"].every((val) => Object.keys(form[0].elements).indexOf(val) > -1)) {
+            // form.attr("action", '/login');
+            form.attr("method", "POST");
+            console.log("Action set");
+            form.submit((event) => {
+                event.preventDefault();
+                login();
+            })
+        }
+        else {
+            console.log("OOPS!!! Action not set");
+        }
+    }
+    var loginFomValidation = () => {
+        $("input[name=email]").val().trim().length === 0 && !validateEmail($("input[name=email]").val().trim()) ? loginErrors.push('email') : '';
+        $("input[name=password]").val().trim().length === 0 ? loginErrors.push('password') : '';
+        console.log(loginErrors);
+
+        if ($("input[name=email]").val().trim().length > 0 && validateEmail($("input[name=email]").val().trim()) && $("input[name=password]").val().trim().length > 0) {
             return true;
         }
         else {
@@ -11,16 +40,7 @@ $(() => {
         }
     }
     var login = () => {
-        validation() ? console.log('Logged in') : console.log('Validation failed');
+        loginFomValidation() ? console.log('Logged in') : console.log('Validation failed');
     }
-
-    const form = $("#scripteed_login_form");
-    if (form.length === 1 && ["email", "password", "remember_me", "login_button"].every((val) => Object.keys(form[0].elements).indexOf(val) > -1)) {
-        form.attr("action", '/login');
-        form.attr("method", "post");
-        console.log("Action set");
-    }
-    else {
-        console.log("OOPS!!! Action not set");
-    }
+    loginFormSanitisation();
 });
