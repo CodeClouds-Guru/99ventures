5f4cc85c4c5a367038f7f913f1876b27accc761e
cc-sourabh <sourabh.das@codeclouds.in>
2023-03-17 13:54:03 +0530

    Lambda function

---
+++ lambda/sync-surveys/models/survey.js
@@ -0,0 +1,51 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Survey extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Survey.belongsTo(models.SurveyProvider, {
+        foreignKey: 'survey_provider_id',
+      });
+      // Survey.belongsToMany(models.MemberTransaction, {
+      //   through: 'member_surveys',
+      //   timestamps: false,
+      //   foreignKey: 'survey_id',
+      //   otherKey: 'member_transaction_id',
+      // }),
+      Survey.hasMany(models.SurveyQualification, {
+        foreignKey: 'survey_id',
+      });
+    }
+  }
+  Survey.init(
+    {
+      survey_provider_id: DataTypes.BIGINT,
+      loi: DataTypes.FLOAT,
+      cpi: DataTypes.FLOAT,
+      name: DataTypes.STRING,
+      survey_number: DataTypes.STRING,
+      status: DataTypes.STRING,
+      original_json: DataTypes.JSON,
+      created_at: 'TIMESTAMP',
+      updated_at: 'TIMESTAMP',
+      deleted_at: 'TIMESTAMP',
+    },
+    {
+      sequelize,
+      modelName: 'Survey',
+      tableName: 'surveys',
+      timestamps: true,
+      paranoid: true,
+      createdAt: 'created_at', // alias createdAt as created_date
+      updatedAt: 'updated_at',
+      deletedAt: 'deleted_at',
+    }
+  );
+  return Survey;
+};
