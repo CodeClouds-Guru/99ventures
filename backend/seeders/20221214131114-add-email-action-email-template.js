9135dc38ace6a4c1930b1f43a807f14065a586bf
CODECLOUDS-DEBOSMITA <codeclouds-debosmita@CISPL-IO-659.local>
2023-04-04 17:36:41 +0530

    commit for email template seeder changes

--- backend/seeders/20221214131114-add-email-action-email-template.js
+++ backend/seeders/20221214131114-add-email-action-email-template.js
@@ -8,18 +8,18 @@ module.exports = {
     await queryInterface.bulkInsert(
       'email_action_email_template',
       [
-        { email_action_id: '1', email_template_id: '18' },
-        { email_action_id: '1', email_template_id: '23' },
-        { email_action_id: '2', email_template_id: '16' },
-        { email_action_id: '2', email_template_id: '21' },
+        { email_action_id: '1', email_template_id: '6' },
+        { email_action_id: '1', email_template_id: '11' },
+        { email_action_id: '2', email_template_id: '4' },
+        { email_action_id: '2', email_template_id: '9' },
         { email_action_id: '3', email_template_id: '1' },
         { email_action_id: '3', email_template_id: '2' },
-        { email_action_id: '8', email_template_id: '14' },
-        { email_action_id: '8', email_template_id: '20' },
-        { email_action_id: '9', email_template_id: '17' },
-        { email_action_id: '9', email_template_id: '22' },
-        { email_action_id: '10', email_template_id: '19' },
-        { email_action_id: '10', email_template_id: '24' },
+        { email_action_id: '8', email_template_id: '8' },
+        { email_action_id: '8', email_template_id: '3' },
+        { email_action_id: '9', email_template_id: '10' },
+        { email_action_id: '9', email_template_id: '5' },
+        { email_action_id: '10', email_template_id: '7' },
+        { email_action_id: '10', email_template_id: '12' },
       ],
       {}
     );
