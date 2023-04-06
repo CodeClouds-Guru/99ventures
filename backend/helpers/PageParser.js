519c7d33d8c8e7697b6dfebea1a1720be9e06e1d
cc-sourabh <sourabh.das@codeclouds.in>
2023-04-06 20:05:07 +0530

    Added if condition helper

--- backend/helpers/PageParser.js
+++ backend/helpers/PageParser.js
@@ -33,6 +33,35 @@ class PageParser {
     Handlebars.registerHelper("format", function (options) {
       return options.fn(this) ? moment(options.fn(this)).format('llll') : '';
     });
+    Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+      switch (operator) {
+        case '==':
+          return (v1 == v2) ? options.fn(this) : options.inverse(this);
+        case '===':
+          return (v1 === v2) ? options.fn(this) : options.inverse(this);
+        case '!=':
+          return (v1 != v2) ? options.fn(this) : options.inverse(this);
+        case '!==':
+          return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+        case '<':
+          return (v1 < v2) ? options.fn(this) : options.inverse(this);
+        case '<=':
+          return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        case '>':
+          return (v1 > v2) ? options.fn(this) : options.inverse(this);
+        case '>=':
+          return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+        case '&&':
+          return (v1 && v2) ? options.fn(this) : options.inverse(this);
+        case '||':
+          return (v1 || v2) ? options.fn(this) : options.inverse(this);
+        default:
+          return options.inverse(this);
+      }
+    });
+    Handlebars.registerHelper('ifNotEquals', function (arg1, arg2, options) {
+      return (arg1 !== arg2) ? options.fn(this) : options.inverse(this);
+    });
   }
 
   async getPageNLayout() {
