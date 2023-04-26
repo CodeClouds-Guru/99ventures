f517847dd4c4636b1f8cb30175871c00cb34b889
Sourav Tapadar <sourav.tapadar@codeclouds.in>
2023-04-26 12:35:36 +0530

    Member withdrawl module change

--- admin/src/app/main/members/Withdraws.js
+++ admin/src/app/main/members/Withdraws.js
@@ -10,7 +10,7 @@ const UserDetails = () => {
     const isMobile = useThemeMediaQuery((theme) => theme.breakpoints.down('lg'));
     const module = 'Withdraws';
     const { moduleId } = useParams();
- 
+
     return (
         <FusePageCarded
             className="sm:px-20"
@@ -19,16 +19,16 @@ const UserDetails = () => {
             }
             content={
                 <Box className="sm:p-16 lg:p-22 md:p-16 xl:p-32 flex flex-col w-full" >
-                    <List module="member-transactions"
-                        where={{member_id: moduleId, type: "withdraw"}}
-                        editable={ false }
-                        addable={ false }
-                        deletable={ false }
-                        moduleHeading={ false }
+                    <List module="withdrawal-requests"
+                        where={{ member_id: moduleId }}
+                        editable={false}
+                        addable={false}
+                        deletable={false}
+                        moduleHeading={false}
                     />
                 </Box>
             }
-            rightSidebarOpen={ false }
+            rightSidebarOpen={false}
             scroll={isMobile ? 'normal' : 'content'}
         />
     );
