e24cbaa89176b1f515b89a7048a7d5e6aa8bc124
cc-sourabh <sourabh.das@codeclouds.in>
2023-04-26 12:20:37 +0530

    reverted back test code

--- lambda/sync-surveys/index.js
+++ lambda/sync-surveys/index.js
@@ -1,6 +1,6 @@
 const Lucid = require("./handlers/Lucid");
-const db = require('./models/index');
 const Schlesinger = require('./handlers/Schlesinger');
+const db = require('./models/index');
 
 const main = async (event) => {
 
@@ -28,20 +28,20 @@ const main = async (event) => {
   return true;
 }
 
-main({
-  Records: [{
-    body: '{\"cpi\": 1, \"is_live\": true, \"industry\": \"other\", \"survey_id\": 33245195, \"conversion\": 0.182403, \"study_type\": \"adhoc\", \"survey_name\": \"Other Survey \", \"account_name\": \"Probity Research\", \"collects_pii\": false, \"bid_incidence\": 90, \"survey_quotas\": [{\"questions\": [], \"quota_cpi\": 1, \"conversion\": 0.182403, \"survey_quota_id\": 169589010, \"survey_quota_type\": \"Total\", \"number_of_respondents\": 568}], \"message_reason\": \"activated\", \"respondent_pids\": [], \"total_remaining\": 568, \"country_language\": \"eng_us\", \"survey_group_ids\": [2953748], \"mobile_conversion\": 0.11, \"overall_completes\": 170, \"earnings_per_click\": 0.182403, \"survey_provider_id\": 1, \"length_of_interview\": 33, \"completion_percentage\": 0.621333, \"survey_qualifications\": [{\"precodes\": [\"18\", \"19\", \"20\", \"21\", \"22\", \"23\", \"24\", \"25\", \"26\", \"27\", \"28\", \"29\", \"30\", \"31\", \"32\", \"33\", \"34\", \"35\", \"36\", \"37\", \"38\", \"39\", \"40\", \"41\", \"42\", \"43\", \"44\", \"45\", \"46\", \"47\", \"48\", \"49\", \"50\", \"51\", \"52\", \"53\", \"54\", \"55\", \"56\", \"57\", \"58\", \"59\", \"60\", \"61\", \"62\", \"63\", \"64\", \"65\", \"66\", \"67\", \"68\", \"69\", \"70\", \"71\", \"72\", \"73\", \"74\", \"75\", \"76\", \"77\", \"78\", \"79\", \"80\", \"81\", \"82\", \"83\", \"84\", \"85\", \"86\", \"87\", \"88\", \"89\", \"90\", \"91\", \"92\", \"93\", \"94\", \"95\", \"96\", \"97\", \"98\", \"99\"], \"question_id\": 42, \"logical_operator\": \"OR\"}, {\"precodes\": [\"1\", \"2\"], \"question_id\": 43, \"logical_operator\": \"OR\"}], \"total_client_entrants\": 932, \"survey_quota_calc_type\": \"prescreens\", \"bid_length_of_interview\": 20, \"is_only_supplier_in_group\": false, \"termination_length_of_interview\": 1}'
-  }]
-})
+// main({
+//   Records: [{
+//     body: '{\"cpi\": 1, \"is_live\": true, \"industry\": \"other\", \"survey_id\": 33245195, \"conversion\": 0.182403, \"study_type\": \"adhoc\", \"survey_name\": \"Other Survey \", \"account_name\": \"Probity Research\", \"collects_pii\": false, \"bid_incidence\": 90, \"survey_quotas\": [{\"questions\": [], \"quota_cpi\": 1, \"conversion\": 0.182403, \"survey_quota_id\": 169589010, \"survey_quota_type\": \"Total\", \"number_of_respondents\": 568}], \"message_reason\": \"activated\", \"respondent_pids\": [], \"total_remaining\": 568, \"country_language\": \"eng_us\", \"survey_group_ids\": [2953748], \"mobile_conversion\": 0.11, \"overall_completes\": 170, \"earnings_per_click\": 0.182403, \"survey_provider_id\": 1, \"length_of_interview\": 33, \"completion_percentage\": 0.621333, \"survey_qualifications\": [{\"precodes\": [\"18\", \"19\", \"20\", \"21\", \"22\", \"23\", \"24\", \"25\", \"26\", \"27\", \"28\", \"29\", \"30\", \"31\", \"32\", \"33\", \"34\", \"35\", \"36\", \"37\", \"38\", \"39\", \"40\", \"41\", \"42\", \"43\", \"44\", \"45\", \"46\", \"47\", \"48\", \"49\", \"50\", \"51\", \"52\", \"53\", \"54\", \"55\", \"56\", \"57\", \"58\", \"59\", \"60\", \"61\", \"62\", \"63\", \"64\", \"65\", \"66\", \"67\", \"68\", \"69\", \"70\", \"71\", \"72\", \"73\", \"74\", \"75\", \"76\", \"77\", \"78\", \"79\", \"80\", \"81\", \"82\", \"83\", \"84\", \"85\", \"86\", \"87\", \"88\", \"89\", \"90\", \"91\", \"92\", \"93\", \"94\", \"95\", \"96\", \"97\", \"98\", \"99\"], \"question_id\": 42, \"logical_operator\": \"OR\"}, {\"precodes\": [\"1\", \"2\"], \"question_id\": 43, \"logical_operator\": \"OR\"}], \"total_client_entrants\": 932, \"survey_quota_calc_type\": \"prescreens\", \"bid_length_of_interview\": 20, \"is_only_supplier_in_group\": false, \"termination_length_of_interview\": 1}'
+//   }]
+// })
 
-// exports.handler = async (event, context) => {
-//   try {
-//     context.callbackWaitsForEmptyEventLoop = false;
-//     return await main(event);
-//   } catch (error) {
-//     console.error(error);
-//   } finally {
-//     await db.sequelize.connectionManager.pool.destroyAllNow();
-//     return true;
-//   }
-// };
+exports.handler = async (event, context) => {
+  try {
+    context.callbackWaitsForEmptyEventLoop = false;
+    return await main(event);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await db.sequelize.connectionManager.pool.destroyAllNow();
+    return true;
+  }
+};
