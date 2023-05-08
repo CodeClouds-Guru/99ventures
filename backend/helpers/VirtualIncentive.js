7668b29ad57cbdbe240ac3ec80af890d1b7f6ffc
Soumen Kamila <soumen.kamila@codeclouds.in>
2023-05-08 19:30:08 +0530

    Working on the Virtual Incentives Create Order API integration

--- backend/helpers/VirtualIncentive.js
+++ backend/helpers/VirtualIncentive.js
@@ -1,21 +1,6 @@
-const axios = require('axios');
-
-const handler = {
-	get(target, prop) {
-		return !(prop in target)
-			? target[prop]
-			: function (...args) {
-				try {
-					return target[prop].apply(this, args);
-				} catch (e) {
-					return {
-						status: false,
-						error: e.message,
-					};
-				}
-			};
-	},
-};
+const programs = require('../config/virtual_incentives.json');
+const { Country, Member } = require('../models/index');
+const {Op} = require('sequelize');
 
 class VirtualIncentive {
 
@@ -28,20 +13,95 @@ class VirtualIncentive {
 		};
         this.baseURL = 'https://rest.virtualincentives.com/v6/json';
 		this.createOrder = this.createOrder.bind(this);
-		return new Proxy(this, handler);
 	}
 
-    async createOrder(payload) {
+    async createOrder(params) {
         try{
-            const instance = axios.create({
-                ...this.instance,
-                baseURL: this.baseURL,
+            const groupData = params.reduce((acc, item )=> {
+                let key = item['program_id'];
+                if (!acc[key]) {
+                    acc[key] = []
+                }
+                acc[key].push(item)
+                return acc
+            }, {});
+
+            const payload = [];
+            Object.keys(groupData).forEach(data =>{
+                const params = {
+                    "order": {
+                        "programid": data,
+                        "clientid": Date.now(),
+                        "fulfillment": (groupData[data].length > 1) ? "bulk" : "individual",        
+                        "accounts": []
+                    }
+                };
+                groupData[data].forEach(row =>{
+                    params.order.accounts.push({
+                        "firstname": row.first_name,
+                        "lastname": row.last_name,
+                        "email": row.email,
+                        "sku": row.sku,
+                        "amount": row.amount     
+                    });
+                });
+                payload.push(params)
+            })
+            console.log(payload)
+            return payload;
+
+
+
+
+            /*const memberIds = params.map(pr => pr.member_id)
+            const members = await Member.findAll({
+                attributes: ['id', 'country_id'],
+                where: {
+                    id: {
+                        [Op.in]: memberIds
+                    }
+                },
+                include: {
+                    model: Country,
+                    attributes: ['iso']
+                }
             });
-            const response = await instance.post('/orders', payload);           
-            return response;
+            
+            const payload = {
+                "order": {
+                    "programid": program.programid,
+                    "clientid": Date.now(),
+                    "fulfillment": (params.length > 1) ? "bulk" : "individual",        
+                    "accounts": []
+                }
+            };
+            members.forEach(member => {
+                let program = programs.find(pr => pr.country === member.country.iso);
+                if(program){
+                    let item = params.find(pr=> pr.member_id === member.id);
+                    payload.order.accounts.push({
+                        "firstname": member.first_name,
+                        "lastname": member.last_name,
+                        "email": member.email,
+                        "sku": item.sku ? item.sku : program.products[0]['sku'],
+                        "amount": item.amount     
+                    });
+                }
+            });
+            if(payload.order.accounts.length) {
+                return payload;
+                const instance = axios.create({
+                    ...this.instance,
+                    baseURL: this.baseURL,
+                });
+                const response = await instance.post('/orders', payload);           
+                return response;
+            } else {
+                throw new Error('Payload');
+            }*/
         } catch(error) {
-            console.error(error.response.data)
-            throw error.response.data;
+            console.error(error)
+            throw error.response;
         }
     }
 
