7d880cd5d7e472a1c4bbd6fb20a1b1b5ae9501ea
CODECLOUDS-DEBOSMITA <codeclouds-debosmita@CISPL-IO-659.local>
2023-05-19 17:28:20 +0530

    commit for notification system modification

--- backend/controllers/backend/WithdrawalRequestController.js
+++ backend/controllers/backend/WithdrawalRequestController.js
@@ -92,7 +92,7 @@ class WithdrawalRequestController extends Controller {
             record.dataValues.Member.dataValues.first_name +
             ' ' +
             record.dataValues.Member.dataValues.last_name;
-            record.dataValues['Member.username'] =
+          record.dataValues['Member.username'] =
             record.dataValues.Member.dataValues.username;
         }
         if (record.dataValues.User != null) {
@@ -221,7 +221,7 @@ class WithdrawalRequestController extends Controller {
               model: WithdrawalType,
               attributes: ['slug', 'payment_method_id'],
             },
-          ]
+          ],
         });
         var items = [];
         var transaction_ids = [];
