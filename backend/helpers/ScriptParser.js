e868f28df5fd72a0524d8df50c53da38ba3bb389
CODECLOUDS-DEBOSMITA <codeclouds-debosmita@CISPL-IO-659.local>
2023-05-25 16:43:11 +0530

    minor issue fixing in helper

--- backend/helpers/ScriptParser.js
+++ backend/helpers/ScriptParser.js
@@ -1,6 +1,6 @@
 const Models = require('../models');
 const Sequelize = require('sequelize');
-const { Op, Model, Model } = require('sequelize');
+const { Op, Model } = require('sequelize');
 const safeEval = require('safe-eval');
 const util = require('util');
 const { ceil } = require('lodash');
@@ -67,7 +67,7 @@ class ScriptParser {
               ...where,
             });
             if (script.module == 'Shoutbox') {
-              data = data.reverse()
+              data = data.reverse();
             }
             // console.log('data', JSON.parse(JSON.stringify(data)));
 
