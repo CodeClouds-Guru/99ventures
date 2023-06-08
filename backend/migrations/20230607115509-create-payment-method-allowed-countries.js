432d9bb9e8118b5ac90af35cb41ffcd23b9c1ed1
CODECLOUDS-DEBOSMITA <codeclouds-debosmita@CISPL-IO-659.local>
2023-06-08 18:36:28 +0530

    bridge table migration modifications

--- backend/migrations/20230607115509-create-payment-method-allowed-countries.js
+++ backend/migrations/20230607115509-create-payment-method-allowed-countries.js
@@ -2,13 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('payment_method_allowed_countries', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
+    await queryInterface.createTable('allowed_countries_payment_method', {
       payment_method_id: {
         type: Sequelize.BIGINT,
       },
@@ -18,6 +12,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('payment_method_allowed_countries');
+    await queryInterface.dropTable('allowed_countries_payment_method');
   },
 };
