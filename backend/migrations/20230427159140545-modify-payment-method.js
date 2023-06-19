c1180cd6b8231738b37617f9c672672be1ca5fb8
CODECLOUDS-DEBOSMITA <codeclouds-debosmita@CISPL-IO-659.local>
2023-06-19 13:08:25 +0530

    migration files removed

--- backend/migrations/20230427159140545-modify-payment-method.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('payment_methods', 'logo');
-  },
-
-  async down(queryInterface, Sequelize) {},
-};
