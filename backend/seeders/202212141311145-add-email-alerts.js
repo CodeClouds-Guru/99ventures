2339087f1b4a3c33a91692757f8c3730fb105bbb
Nandita <nandita.bardhan@codeclouds.co.in>
2023-07-14 18:39:10 +0530

    email alerts

--- backend/seeders/202212141311145-add-email-alerts.js
+++ backend/seeders/202212141311145-add-email-alerts.js
@@ -11,22 +11,27 @@ module.exports = {
         {
           name: 'Completed Rewards',
           slug: 'completed_rewards',
+          description:'Yes, please send an email when I have been successfully credited for a survey/offer.'
         },
         {
           name: 'Completed Withdraw',
           slug: 'completed_withdraw',
+          description:'Yes, please send an email when my withdrawal has been approved.'
         },
         {
           name: 'Notifications',
           slug: 'notifications',
+          description:' Yes, please send an email when I receive an account notification.'
         },
         {
           name: 'Referrals',
           slug: 'referrals',
+          description:'Yes, please send an email for each new referral I receive.'
         },
         {
-          name: 'Email Marketting',
+          name: 'Email Marketing',
           slug: 'email_marketting',
+          description:'Yes, please send an e-mail when my reward is approved and processed.'
         },
       ],
       {}
@@ -40,6 +45,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-    await queryInterface.bulkDelete('email_alerts', null, {});
+    await queryInterface.bulkDelete('email_alerts', null, {truncate: true,cascade: true});
   },
 };
