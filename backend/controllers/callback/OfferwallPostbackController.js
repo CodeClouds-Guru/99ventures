f3d4f2a5cfb23480b0c741f37bf14c2085885427
CODECLOUDS-DEBOSMITA <codeclouds-debosmita@CISPL-IO-659.local>
2023-08-08 19:29:25 +0530

    Offerwall fixes

--- backend/controllers/callback/OfferwallPostbackController.js
+++ backend/controllers/callback/OfferwallPostbackController.js
@@ -6,19 +6,20 @@ const {
 } = require('../../models');
 
 class OfferwallPostbackController {
-  constructor() { }
+  constructor() {}
   async save(req, res) {
-    //---- To Generate Log
     const logger1 = require('../../helpers/Logger')(
       `outcome-${req.params.offerwall}.log`
     );
     logger1.info(req.method);
     logger1.info(JSON.stringify(req.query));
     logger1.info(JSON.stringify(req.body));
-    //----
 
-    // console.log(req.params.offerwall);
-    if (req.query.status === '1' || req.body.status === '1') {
+    var status_arr = ['1', 'approved'];
+    if (
+      status_arr.indexOf(req.query.status) > -1 ||
+      status_arr.indexOf(req.body.status) > -1
+    ) {
       try {
         const offerwall_name = req.params.offerwall;
         let offerwall_details = await OfferWall.findOne({
@@ -31,9 +32,9 @@ class OfferwallPostbackController {
           ],
           where: { name: offerwall_name },
         });
-
         var currency_percent = offerwall_details.currency_percent || '100';
         currency_percent = parseFloat(currency_percent);
+        console.log(currency_percent);
         if (
           offerwall_details &&
           (offerwall_details.campaign_id_variable in req.query ||
@@ -57,7 +58,6 @@ class OfferwallPostbackController {
               attributes: ['id'],
             },
           });
-          // console.log('member', member);
 
           if (member) {
             let payout_amount = 0;
@@ -70,11 +70,11 @@ class OfferwallPostbackController {
                 offerwall_details.campaign_name_variable
               ]
                 ? parseFloat(
-                  req.query[offerwall_details.campaign_name_variable]
-                )
+                    req.query[offerwall_details.campaign_name_variable]
+                  )
                 : parseFloat(
-                  req.body[offerwall_details.campaign_name_variable]
-                );
+                    req.body[offerwall_details.campaign_name_variable]
+                  );
             }
 
             if (
@@ -85,8 +85,8 @@ class OfferwallPostbackController {
                 ? req.query[offerwall_details.sub_id_variable]
                 : req.body[offerwall_details.sub_id_variable];
             }
-
             payout_amount = (payout_amount * currency_percent) / 100;
+            console.log('payout_amount', payout_amount);
             const transaction_obj = {
               member_id: member.id,
               amount: payout_amount,
@@ -97,32 +97,34 @@ class OfferwallPostbackController {
               payload: req.body
                 ? JSON.stringify(req.body)
                 : JSON.stringify(req.query),
-              status: 2
+              status: 2,
             };
-            // console.log('transaction_obj', transaction_obj);
+            console.log('transaction_obj', transaction_obj);
             let result =
               await MemberTransaction.updateMemberTransactionAndBalance(
                 transaction_obj
               );
 
-            res.send(
-              Object.keys(req.body).length ? JSON.stringify(req.body) : JSON.stringify(req.query)
+            return res.send(
+              Object.keys(req.body).length
+                ? JSON.stringify(req.body)
+                : JSON.stringify(req.query)
             );
-            return;
+            // return;
           }
         }
       } catch (err) {
         console.log(err.message);
-        res.status(500).json({
+        return res.status(500).json({
           status: false,
           errors: 'Unable to save data',
         });
       }
     }
-    res.send({
+    return res.send({
       status: true,
-      message: 'Completed'
-    })
+      message: 'Completed',
+    });
   }
 }
 
