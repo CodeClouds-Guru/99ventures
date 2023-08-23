ecf9c46be2b0af6022ba35e702fadc188f59d958
Soumen Kamila <soumen.kamila@codeclouds.in>
2023-08-23 22:16:39 +0530

    Lucid lambda code modified

--- lambda/serverless-offline/functions/lucid.js
+++ lambda/serverless-offline/functions/lucid.js
@@ -9,6 +9,12 @@ class Lucid {
     }
 
     surveySync = async () => {
+        if(this.record.country_id == null){
+            return {
+                'message': 'Unable to locate country id',
+                'status': false
+            }
+        }
         var params = [
             this.record.survey_provider_id,
             this.record.length_of_interview,
@@ -23,17 +29,9 @@ class Lucid {
         ];
         var sql;
 
-        let countrySql = `SELECT id from countries WHERE lucid_language_code = ? AND deleted_at IS NULL LIMIT 1`;
-        const country = await this.db.query(countrySql, [this.record.country_language]);
-        if(country.length < 1){
-            return {
-                'message': 'Unable to locate country id',
-                'status': false
-            }
-        }
 
-        const chkSql = `SELECT id FROM surveys WHERE survey_provider_id = ? AND survey_number = ? AND country_id = ? AND deleted_at IS NULL`;
-        const surveyData = await this.db.query(chkSql, [this.record.survey_provider_id, this.record.survey_id, country[0].id]);
+        const chkSql = `SELECT id FROM surveys WHERE survey_provider_id = ? AND survey_number = ? AND deleted_at IS NULL`;
+        const surveyData = await this.db.query(chkSql, [this.record.survey_provider_id, this.record.survey_id]);
         if (surveyData.length) {
             let surveyId = surveyData[0].id;
             const surveyIds = surveyData.map(sr => sr.id);
@@ -41,17 +39,17 @@ class Lucid {
             let deleteSql = `DELETE surveys, survey_qualifications FROM surveys JOIN survey_qualifications ON (surveys.id = survey_qualifications.survey_id) WHERE surveys.id IN (?)`;
             await this.db.query(deleteSql, [surveyIds]);
 
-            params = [surveyId, ...params, country[0].id];
+            params = [surveyId, ...params, this.record.country_id];
             sql = `INSERT INTO surveys (id, survey_provider_id, loi, cpi, name, created_at, updated_at, survey_number, status, original_json, url, country_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
         } else {
-            params.push(country[0].id);
+            params.push(this.record.country_id);
             sql = `INSERT INTO surveys (survey_provider_id, loi, cpi, name, created_at, updated_at, survey_number, status, original_json, url, country_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
         }
 
         if ((this.record.is_live || this.record.is_live === 'true') && this.record.message_reason !== 'deactivated') {
             const rows = await this.db.query(sql, params);
             // console.log(rows.insertId)
-            await this.surveyQualificationSync(rows.insertId, this.db, country[0].id);
+            await this.surveyQualificationSync(rows.insertId, this.db, this.record.country_id);
             return rows;
         }
         return true;
@@ -89,31 +87,35 @@ class Lucid {
             // let sapSql = 'INSERT IGNORE INTO survey_answer_precodes (`option`, precode) VALUES ?';
             // await db.query(sapSql, [precodesSet]);
 
-            let joinSql = `SELECT sq.id as qualification_id, sq.survey_question_id, qs.survey_provider_question_id, qs.question_type, ap.option, ap.id as answer_precode_id
-            FROM survey_qualifications AS sq JOIN survey_questions AS qs ON (sq.survey_question_id = qs.id)
-            JOIN survey_answer_precodes AS ap ON (ap.precode = qs.survey_provider_question_id)
-            WHERE sq.deleted_at IS NULL 
-            AND qs.deleted_at IS NULL 
-            AND ap.deleted_at IS NULL 
-            AND sq.survey_id = ? 
-            AND ap.country_id = ? 
-            AND ap.survey_provider_id = ?`;
-            const qlData = await db.query(joinSql, [surveyId, country_id, this.record.survey_provider_id]);
 
-            const ansPrecodeParams = []
-            for (const item of this.record.survey_qualifications) {
-                qlData
-                    .filter(row => row.survey_provider_question_id === item.question_id)
+            let sqlQry = `SELECT sq.id as qualification_id, sq.survey_question_id, qs.survey_provider_question_id, qs.question_type
+                FROM survey_qualifications AS sq JOIN survey_questions AS qs ON (sq.survey_question_id = qs.id)                
+                WHERE sq.deleted_at IS NULL
+                AND qs.deleted_at IS NULL 
+                AND sq.survey_id = ?`;
+            const qlData = await db.query(sqlQry, [surveyId]);
+            
+            const precodes = this.record.survey_qualifications.map(r=> r.question_id);
+            let ansPQl = `Select ap.id, ap.precode, ap.option from survey_answer_precodes AS ap where 
+            ap.precode IN (?) AND 
+            ap.survey_provider_id = ? AND 
+            ap.country_id = ?`;
+            const answerPrecodes = await db.query(ansPQl, [precodes, this.record.survey_provider_id, country_id]);
+            
+            const ansPrecodeParams = [];
+            for (const item of this.record.survey_qualifications) 
+            {
+                let data = qlData.find(r => +item.question_id === +r.survey_provider_question_id);
+                answerPrecodes
+                    .filter(row => +row.precode === +item.question_id)
                     .filter(opt => item.precodes.includes(opt.option))
                     .forEach(el => {
                         ansPrecodeParams.push([
-                            el.qualification_id,
-                            el.answer_precode_id
+                            data.qualification_id,
+                            el.id
                         ]);
                     });
             }
-
-            // console.log(ansPrecodeParams);
 
             let sapsqSql = `INSERT INTO survey_answer_precode_survey_qualifications (survey_qualification_id, survey_answer_precode_id) VALUES ?`;
             await db.query(sapsqSql, [ansPrecodeParams]);
