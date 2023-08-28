5640f631c24ccc57a7adc87a7ff0414abf8d810d
Soumen Kamila <soumen.kamila@codeclouds.in>
2023-08-28 23:17:01 +0530

    Lucid survey filtered and based on that qualifications are passing to the URL

--- backend/controllers/frontend/LucidController.js
+++ backend/controllers/frontend/LucidController.js
@@ -21,6 +21,76 @@ class LucidController {
         this.addEntryLink = this.addEntryLink.bind(this);
     }
 
+    /*surveys1 = async (req, res) => {
+        const eligibilities = await MemberEligibilities.getEligibilities(226, 1, 160);
+        const surveys = await Survey.findAndCountAll({
+            attributes: ['id', 'survey_provider_id', 'loi', 'cpi', 'survey_number', 'created_at', 'name'],
+            distinct: true,
+            where: {
+                survey_provider_id: 1,
+                status: "active",
+                country_id: 226
+            },
+            include: {
+                model: SurveyQualification,
+                attributes: ['id', 'survey_question_id'],
+                where: {
+                    survey_question_id: [109011, 109010, 109012, 109013]
+                },
+                required: true,
+                include: {
+                    model: SurveyAnswerPrecodes,
+                    attributes: ['id', 'option', 'precode'],
+                    required: true,
+                    // include: [
+                    //     {
+                    //         model: SurveyQuestion,
+                    //         attributes: ['id'],
+                    //         where: {
+                    //             id: [109011, 109010, 109012]
+                    //         }
+                    //     }
+                    // ],
+                }
+            },
+            order: [[sequelize.literal('created_at'), 'desc']],
+            limit: 100,
+            offset: (1 - 1) * 10,
+        });
+
+        const surveyData = [];
+
+        surveys.rows.forEach((record, index) => {
+            let findAnsResult = [];
+            record.SurveyQualifications.forEach(r=> {
+                let findQs = eligibilities.find(t=> t.survey_question_id == r.survey_question_id);
+                if(findQs !== undefined){
+                    let findAns = r.SurveyAnswerPrecodes.find(j=> findQs.survey_answer_precode_id == j.id);
+                    if(findAns === undefined){
+                        findAnsResult = [];
+                    } else 
+                        findAnsResult.push(findAns);                    
+                }                
+            });
+            // console.log(findAnsResult.length +'==='+ record.SurveyQualifications.length)
+            if(findAnsResult.length === record.SurveyQualifications.length)
+                surveyData.push(record);
+        });
+        const queryString = {};
+        for (let survey of surveyData) {
+            survey.SurveyQualifications.find(r=> {
+                let findEl = eligibilities.find(el => el.survey_question_id == r.survey_question_id);
+                if(findEl !== undefined) {
+                    queryString[findEl.survey_provider_question_id] = findEl.option
+                }
+            });
+            console.log(queryString)
+        }
+        res.send(surveyData);
+        return;
+
+    }*/
+
     surveys = async (memberId, params) => {
         try{
             const member = await Member.findOne({
@@ -53,7 +123,6 @@ class LucidController {
             }
             
             const pageNo = 'pageno' in params ? parseInt(params.pageno) : 1;
-            //const perPage = 100;
             const perPage = 'perpage' in params ? parseInt(params.perpage) : 12;
             const orderBy = 'orderby' in params ? params.orderby : 'created_at';
             const order = 'order' in params ? params.order : 'desc';
@@ -74,13 +143,16 @@ class LucidController {
             const matchingQuestionIds = [];
             const matchingAnswerIds = [];
             eligibilities.forEach(eg => {
-                queryString[eg.survey_provider_question_id] = eg.option ? eg.option : eg.open_ended_value;
+                // queryString[eg.survey_provider_question_id] = eg.option ? eg.option : eg.open_ended_value;
                 matchingQuestionIds.push(eg.survey_question_id);
                 if(eg.survey_answer_precode_id !== null){                    
                     matchingAnswerIds.push(+eg.survey_answer_precode_id);
                 }
+                if(eg.open_ended_value) {
+                    queryString[eg.survey_provider_question_id] = eg.open_ended_value;
+                }
             });
-            const generateQueryString = new URLSearchParams(queryString).toString();
+            //const generateQueryString = new URLSearchParams(queryString).toString();
             /** End */
             
             if (matchingAnswerIds.length && matchingQuestionIds.length) {
@@ -105,19 +177,13 @@ class LucidController {
                         model: SurveyQualification,
                         attributes: ['id', 'survey_question_id'],
                         required: true,
+                        where: {
+                            survey_question_id: matchingQuestionIds
+                        },
                         include: {
                             model: SurveyAnswerPrecodes,
                             attributes: ['id', 'option', 'precode'],
-                            required: true,
-                            include: [
-                                {
-                                    model: SurveyQuestion,
-                                    attributes: ['id'],
-                                    where: {
-                                        id: matchingQuestionIds
-                                    }
-                                }
-                            ],
+                            required: true
                         }
                     },
                     order: [[sequelize.literal(orderBy), order]],
@@ -131,21 +197,44 @@ class LucidController {
                         message: 'No matching surveys!'
                     }
                 }
-                const surveyData = [];
+                const surveyData = [];                
                 surveys.rows.forEach((record, index) => {
-                    const qual = record.SurveyQualifications.find(r => matchingQuestionIds.includes(r.survey_question_id));
-                    if(qual !== undefined) {
-                        let answerPrecode = qual.SurveyAnswerPrecodes.some(r=> matchingAnswerIds.includes(r.id));
-                        if(answerPrecode) {
-                            surveyData.push(record)
-                        }
+                    let findAnsResult = [];
+                    record.SurveyQualifications.forEach(r=> {
+                        let findQs = eligibilities.find(t=> t.survey_question_id == r.survey_question_id);
+                        if(findQs !== undefined){
+                            let findAns = r.SurveyAnswerPrecodes.find(j=> findQs.survey_answer_precode_id == j.id);
+                            if(findAns === undefined){
+                                findAnsResult = [];
+                            } else 
+                                findAnsResult.push(findAns);                    
+                        }                
+                    });
+                    if(findAnsResult.length === record.SurveyQualifications.length){
+                        surveyData.push(record);
                     }
-                })
+                });
+                // surveys.rows.forEach((record, index) => {
+                //     const qual = record.SurveyQualifications.find(r => matchingQuestionIds.includes(r.survey_question_id));
+                //     if(qual !== undefined) {
+                //         let answerPrecode = qual.SurveyAnswerPrecodes.some(r=> matchingAnswerIds.includes(r.id));
+                //         if(answerPrecode) {
+                //             surveyData.push(record)
+                //         }
+                //     }
+                // })
 
                 var page_count = Math.ceil(surveys.count / perPage);
                 var survey_list = [];                
                 if(surveyData && surveyData.length){
                     for (let survey of surveyData) {
+                        survey.SurveyQualifications.find(r=> {
+                            let findEl = eligibilities.find(el => el.survey_question_id == r.survey_question_id);
+                            if(findEl !== undefined) {
+                                queryString[findEl.survey_provider_question_id] = findEl.option
+                            }
+                        });
+                        let generateQueryString = new URLSearchParams(queryString).toString();
                         let link = `/lucid/entrylink?survey_number=${survey.survey_number}&uid=${member.username}&${generateQueryString}`;
                         let temp_survey = {
                             survey_number: survey.survey_number,
