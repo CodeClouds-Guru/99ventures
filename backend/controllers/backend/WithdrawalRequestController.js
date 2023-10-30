7ac1b7bacac944034c164c0816deec4946630400
CODECLOUDS-DEBOSMITA <codeclouds-debosmita@CISPL-IO-659.local>
2023-10-30 15:46:11 +0530

    commit for admin withdrawal pending and reversal logic11

--- backend/controllers/backend/WithdrawalRequestController.js
+++ backend/controllers/backend/WithdrawalRequestController.js
@@ -113,12 +113,12 @@ class WithdrawalRequestController extends Controller {
 
         //check if any reversal happened after withdraw req
         let query =
-          'SELECT COUNT(id) from member_transactions where member_id =? and amount_action = "reversed_transaction" and status = 5 and created_at > (select created_at from withdrawal_requests where member_id = ? and status = "pending" order by created_at limit 0,1)';
+          'SELECT COUNT(id) as reverse_count from member_transactions where member_id =? and amount_action = "reversed_transaction" and status = 5 and created_at > (select created_at from withdrawal_requests where member_id = ? and status = "pending" order by created_at limit 0,1)';
         let reversal_transaction = await db.sequelize.query(query, {
           replacements: [row.member_id, row.member_id],
           type: QueryTypes.SELECT,
         });
-        console.log('reversal_transaction', reversal_transaction);
+        console.log('reversal_transaction', reversal_transaction[0]);
         let warning_text =
           reversal_transaction > 0
             ? 'This user received a reversed transaction. Please be carefull before approving the request!'
