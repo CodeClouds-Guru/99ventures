9cd2c8008de72a46126267d7ee5ffec7f562ae87
CODECLOUDS-DEBOSMITA <codeclouds-debosmita@CISPL-IO-659.local>
2023-12-07 13:33:25 +0530

    commit for new migrations

---
+++ backend/migrations/20231206179140557-create-survey-attempt.js
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('survey_attempts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      survey_number: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      member_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+      },
+      survey_provider_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: 'TIMESTAMP',
+      },
+      updated_at: {
+        type: 'TIMESTAMP',
+      },
+      deleted_at: {
+        type: 'TIMESTAMP',
+      },
+    });
+    await queryInterface.addIndex(
+      'survey_number_member_id_survey_provider_id',
+      {
+        fields: ['survey_number', 'member_id', 'survey_provider_id'],
+        unique: true,
+      }
+    );
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeIndex(
+      'survey_attempts',
+      'survey_number_member_id_survey_provider_id'
+    );
+    await queryInterface.dropTable('survey_attempts');
+  },
+};
