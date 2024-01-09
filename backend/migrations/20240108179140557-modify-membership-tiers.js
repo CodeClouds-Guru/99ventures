e3071dfb1ae1e8f576890d276e3f8e6490f93f1d
CODECLOUDS-DEBOSMITA <codeclouds-debosmita@CISPL-IO-659.local>
2024-01-09 12:15:25 +0530

    commit for adding tier module

---
+++ backend/migrations/20240108179140557-modify-membership-tiers.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('membership_tiers', 'logo', {
+      type: Sequelize.TEXT,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('membership_tiers', 'created_at', {
+      type: 'TIMESTAMP',
+    });
+    await queryInterface.addColumn('membership_tiers', 'updated_at', {
+      type: 'TIMESTAMP',
+    });
+    await queryInterface.addColumn('membership_tiers', 'deleted_at', {
+      type: 'TIMESTAMP',
+    });
+    await queryInterface.addColumn('membership_tiers', 'created_by', {
+      type: Sequelize.TINYINT,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('membership_tiers', 'updated_by', {
+      type: Sequelize.TINYINT,
+      allowNull: true,
+    });
+    await queryInterface.addColumn('membership_tiers', 'deleted_by', {
+      type: Sequelize.TINYINT,
+      allowNull: true,
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('membership_tiers', 'logo');
+  },
+};
