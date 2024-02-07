9c95bf41d3319772d78b6f1f054fc182b2d4182f
Soumen Kamila <soumen.kamila@codeclouds.in>
2024-02-07 14:08:21 +0530

    Updated survey group checking for Lucid

--- backend/controllers/frontend/LucidController.js
+++ backend/controllers/frontend/LucidController.js
@@ -21,7 +21,9 @@ class LucidController {
 		this.rebuildEntryLink = this.rebuildEntryLink.bind(this);
 		this.addEntryLink = this.addEntryLink.bind(this);
 		this.generateQueryString = this.generateQueryString.bind(this);
-		this.surveyGroupCheckAndSurveyAttemptEntry = this.surveyGroupCheckAndSurveyAttemptEntry.bind(this);
+		// this.surveyGroupCheckAndSurveyAttemptEntry = this.surveyGroupCheckAndSurveyAttemptEntry.bind(this);
+		this.fetchSurveyGroups = this.fetchSurveyGroups.bind(this);
+		this.insertSurveyGroupsIds = this.insertSurveyGroupsIds.bind(this);
 	}
 
 	surveys = async (memberId, params, req) => {
@@ -163,7 +165,277 @@ class LucidController {
 		}
 	};
 
-	surveysOld = async (memberId, params, req) => {
+	generateEntryLink = async (req, res) => {
+		if (!req.session.member) {
+			res.redirect('/login');
+			return;
+		}
+
+		const lcObj = new LucidHelper();
+		const surveyNumber = req.query.survey_number;
+		const member = await Member.findOne({
+			attributes: ['id'],
+			where: {
+				username: req.query.uid,
+			},
+			include: {
+				model: CompanyPortal,
+				attributes: ['name'],
+			}
+		});
+		
+		if (member === null) {
+			req.session.flash = { error: 'Sorry! Member not found!' };
+			res.redirect('back');
+			return;
+		}
+		const survey = await Survey.findOne({
+			attributes: ['url', 'original_json'],
+			where: {
+				survey_number: surveyNumber,
+				survey_provider_id: 1,
+			}
+		});
+
+		if (survey === null) {
+			req.session.flash = { error: 'Sorry! survey not found!' };
+			res.redirect('back');
+			return;
+		}
+
+		const surveyGroupIds = await this.fetchSurveyGroups(surveyNumber);
+		try {
+			// This block will check whether this survey and survey groups already attempted by the member
+			const recordCount = await SurveyAttempt.count({
+				where: {
+					member_id: member.id,
+					survey_number: {
+						[Op.in]: surveyGroupIds
+					}
+				}
+			});
+			if(recordCount != 0 ) {
+				req.session.flash = { error: 'You have already attempted to this survey!' };
+				res.redirect('back');
+				return;
+			}
+
+			const quota = await lcObj.showQuota(surveyNumber);
+			if (quota.SurveyStillLive == true || quota.SurveyStillLive == 'true') {
+				const queryParams = req.query;
+				const pid = generateUserIdForSurveyProviders(member.CompanyPortal.name, member.id);
+				const params = {
+					MID: Date.now(),
+					PID: pid,
+					...queryParams
+				};
+				delete params.survey_number;
+				delete params.uid;
+				var entryLink;
+				if (survey && survey.url !== null) {
+					entryLink = survey.url;
+				} else {
+					try {
+						//Sometimes the survey entrylink not created and API sending 404 response.
+						//That's why making this survey to draft as catch block
+						const result = await lcObj.createEntryLink(surveyNumber);
+						if (result.data && result.data.SupplierLink) {
+							const url =
+								process.env.DEV_MODE == '0'
+									? result.data.SupplierLink.LiveLink
+									: result.data.SupplierLink.TestLink;
+							await this.addEntryLink(surveyNumber, url);
+							entryLink = url;
+						}
+					} catch (error) {
+						//If entry link already created but unable to find from DB
+						//that's why API has been used to get the entry link and again store in DB
+						if (error.response.status === 409) {
+							const result = await lcObj.getEntryLink(surveyNumber);
+							if (result.ResultCount !== null && result.SupplierLink) {
+								const url =
+									process.env.DEV_MODE == '0'
+										? result.SupplierLink.LiveLink
+										: result.SupplierLink.TestLink;
+								await this.addEntryLink(surveyNumber, url);
+								entryLink = url;
+							}
+						} else {
+							throw {
+								survey_number: surveyNumber,
+								message: 'Sorry! Survey not found.',
+							};
+						}
+					}
+				}
+				let URL = this.rebuildEntryLink(entryLink, params);
+				// console.log('Lucid entry link', URL);
+				res.redirect(URL);
+				return;
+			} else {
+				throw {
+					survey_number: surveyNumber,
+					message: 'Sorry! Survey is not live now.',
+				};
+			}			
+		} catch (error) {
+			console.log(error)
+			if ('survey_number' in error && error.survey_number) {
+				await Survey.update(
+					{
+						status: 'draft',
+						deleted_at: new Date(),
+					},
+					{
+						where: {
+							survey_number: error.survey_number,
+						},
+					}
+				);
+			} else {
+				const logger = require('../../helpers/Logger')(
+					`lucid-errror.log`
+				);
+				logger.error(error);
+			}
+			res.redirect('/survey-notavailable');
+		} finally {
+			// insert survey groups
+			await this.insertSurveyGroupsIds(surveyGroupIds, member.id);
+		}
+	};
+
+	/**
+	 * Rebuild the entry link with hash
+	 * NOTE: When hashing URLs the base string should include the entire URL, up to and including the `&` preceding the hashing parameter.
+	 * For more details visit https://partners.lucidhq.com/s/security-sha-1-hashing
+	 */
+	rebuildEntryLink = (url, queryParams) => {
+		if (process.env.DEV_MODE == '0') {
+			// 0 = live
+			var url = url.replace('&PID=', '');
+			const params = new URLSearchParams(queryParams).toString();
+			const urlTobeHashed = url + '&' + params + '&';
+			const hash = generateHashForLucid(urlTobeHashed);
+			return url + '&' + params + '&hash=' + hash;
+		} else {
+			delete queryParams.PID;
+			const params = new URLSearchParams(queryParams).toString();
+			const urlTobeHashed = url + '&' + params + '&';
+			const hash = generateHashForLucid(urlTobeHashed);
+			return url + '&' + params + '&hash=' + hash;
+		}
+	};
+
+	addEntryLink = async (surveyNumber, url) => {
+		await sequelize.query(
+			'UPDATE surveys SET url = :url WHERE survey_number = :survey_number AND survey_provider_id = :provider_id',
+			{
+				replacements: { url: url, survey_number: surveyNumber, provider_id: 1 },
+				type: QueryTypes.UPDATE,
+			}
+		);
+		return true;
+	};
+
+	generateQueryString = (queryString, qualifications, eligibilities) => {
+		let quesStr = {};
+		let limit = 5; // 5 is set because Lucid have a limitation to set the querystring in the URL.
+		let countStr = Object.keys(queryString).length;
+		for (let qual of qualifications) {
+			let findEl = eligibilities.find(
+				(el) => el.survey_question_id == qual.survey_question_id
+			);
+			if (findEl !== undefined) {
+				quesStr[findEl.survey_provider_question_id] = findEl.option;
+			}
+
+			if (Object.keys(quesStr).length + countStr === limit) {
+				break;
+			}
+		}
+		return new URLSearchParams({ ...queryString, ...quesStr }).toString();
+	};
+
+	/**
+	 * Check survey group and inserted all the group surveys to survey_attempt table
+	 */
+	/*surveyGroupCheckAndSurveyAttemptEntry = async(surveyNumber, surveyGroupId, memberId) => {
+		try {
+			const lcObj = new LucidHelper();
+			const getData = await lcObj.getSurveyGroups(surveyNumber);
+			const groupIds = [surveyNumber];
+			if (getData.ResultCount != 0 && getData.SurveyGroups.length) {				
+				getData.SurveyGroups.forEach(row => {
+					if (surveyGroupId.includes(row.SurveyGroupID)) {
+						groupIds.push(...groupIds, row.SurveyGroupID, ...row.SurveyGroupSurveys);
+					}
+				});				
+			}
+			if (groupIds.length) {
+				const params = [];
+				groupIds.forEach(row => {
+					params.push({
+						survey_number: row,
+						member_id: memberId,
+						survey_provider_id: 1
+					});
+				});
+				await SurveyAttempt.bulkCreate(params, {
+					ignoreDuplicates: true
+				});
+			}
+		}
+		catch (error) { }
+
+		return;
+	}*/
+
+	/**
+	 * Survey group checking by using survey number.
+	 * If survey group not exists then existing survey number will return 
+	 */
+	fetchSurveyGroups = async(surveyNumber)=>{
+		try{
+			const lcObj = new LucidHelper();
+			const getData = await lcObj.getSurveyGroups(surveyNumber);
+			const groupIds = [];
+			if (getData.ResultCount != 0 && getData.SurveyGroups.length) {
+				getData.SurveyGroups.forEach(row => {
+					//if (surveyGroupId.includes(row.SurveyGroupID)) {
+						groupIds.push(row.SurveyGroupID, ...row.SurveyGroupSurveys);
+					//}
+				});				
+			} else {
+				groupIds.push(surveyNumber);
+			}
+			return groupIds;
+		}
+		catch(error){
+			const logger = require('../../helpers/Logger')(
+				`lucid-errror.log`
+			);
+			logger.error('[survey group error]');
+			logger.error(error);
+			return [surveyNumber];
+		}
+	}
+
+	insertSurveyGroupsIds = async(groupIds, memberId) => {
+		const params = [];
+		groupIds.forEach(row => {
+			params.push({
+				survey_number: row,
+				member_id: memberId,
+				survey_provider_id: 1
+			});
+		});
+		await SurveyAttempt.bulkCreate(params, {
+			ignoreDuplicates: true
+		});
+	}
+
+	/*surveysOld = async (memberId, params, req) => {
 		try {
 			const member = await Member.findOne({
 				attributes: ['username', 'country_id'],
@@ -201,7 +473,7 @@ class LucidController {
 
 			/**
 			 * check and get member's eligibility
-			 */
+			 *
 			const eligibilities = await MemberEligibilities.getEligibilities(
 				member.country_id,
 				provider.id,
@@ -214,7 +486,7 @@ class LucidController {
 					message: 'Sorry! you are not eligible.',
 				};
 			}
-			/** Query String Formation Start */
+			/** Query String Formation Start *
 			const queryString = {};
 			const matchingQuestionIds = [];
 			const matchingAnswerIds = [];
@@ -229,7 +501,7 @@ class LucidController {
 				}
 			});
 			//const generateQueryString = new URLSearchParams(queryString).toString();
-			/** End */
+			/** End *
 
 			if (matchingAnswerIds.length && matchingQuestionIds.length) {
 				const acceptedSurveys = await Member.acceptedSurveys(
@@ -351,228 +623,7 @@ class LucidController {
 				message: 'Something went wrong!',
 			};
 		}
-	};
-
-	generateEntryLink = async (req, res) => {
-		if (!req.session.member) {
-			res.redirect('/login');
-			return;
-		}
-
-		const lcObj = new LucidHelper();
-		const surveyNumber = req.query.survey_number;
-		const member = await Member.findOne({
-			attributes: ['id'],
-			where: {
-				username: req.query.uid,
-			},
-			include: {
-				model: CompanyPortal,
-				attributes: ['name'],
-			}
-		});
-		if (member === null) {
-			req.session.flash = { error: 'Sorry! Member not found!' };
-			res.redirect('back');
-			return;
-		}
-		const survey = await Survey.findOne({
-			attributes: ['url', 'original_json'],
-			where: {
-				survey_number: surveyNumber,
-				survey_provider_id: 1,
-			}
-		});
-
-		if (survey === null) {
-			req.session.flash = { error: 'Sorry! survey not found!' };
-			res.redirect('back');
-			return;
-		}
-		
-		try {
-			// This block will check whether the member has already attempted to this survey or not
-			const result = await Survey.surveyAttemptedByUser(member.id, surveyNumber, 1);
-			if (result.length) {
-				req.session.flash = { error: 'You have already attempted to this survey!' };
-				res.redirect('back');
-				return;
-			}
-
-			const quota = await lcObj.showQuota(surveyNumber);
-			if (quota.SurveyStillLive == true || quota.SurveyStillLive == 'true') {
-				const queryParams = req.query;
-				const pid = generateUserIdForSurveyProviders(member.CompanyPortal.name, member.id);
-				const params = {
-					MID: Date.now(),
-					PID: pid,
-					...queryParams
-				};
-				delete params.survey_number;
-				delete params.uid;
-				var entryLink;
-				if (survey && survey.url !== null) {
-					entryLink = survey.url;
-				} else {
-					try {
-						//Sometimes the survey entrylink not created and API sending 404 response.
-						//That's why making this survey to draft as catch block
-						const result = await lcObj.createEntryLink(surveyNumber);
-						if (result.data && result.data.SupplierLink) {
-							const url =
-								process.env.DEV_MODE == '0'
-									? result.data.SupplierLink.LiveLink
-									: result.data.SupplierLink.TestLink;
-							await this.addEntryLink(surveyNumber, url);
-							entryLink = url;
-						}
-					} catch (error) {
-						//If entry link already created but unable to find from DB
-						//that's why API has been used to get the entry link and again store in DB
-						if (error.response.status === 409) {
-							const result = await lcObj.getEntryLink(surveyNumber);
-							if (result.ResultCount !== null && result.SupplierLink) {
-								const url =
-									process.env.DEV_MODE == '0'
-										? result.SupplierLink.LiveLink
-										: result.SupplierLink.TestLink;
-								await this.addEntryLink(surveyNumber, url);
-								entryLink = url;
-							}
-						} else {
-							throw {
-								survey_number: surveyNumber,
-								message: 'Sorry! Survey not found.',
-							};
-						}
-					}
-				}
-				let URL = this.rebuildEntryLink(entryLink, params);
-				// console.log('Lucid entry link', URL);
-				res.redirect(URL);
-				return;
-			} else {
-				throw {
-					survey_number: surveyNumber,
-					message: 'Sorry! Survey is not live now.',
-				};
-			}
-		} catch (error) {
-			console.log(error)
-			if ('survey_number' in error && error.survey_number) {
-				await Survey.update(
-					{
-						status: 'draft',
-						deleted_at: new Date(),
-					},
-					{
-						where: {
-							survey_number: error.survey_number,
-						},
-					}
-				);
-			} else {
-				const logger = require('../../helpers/Logger')(
-					`lucid-errror.log`
-				);
-				logger.error(error);
-			}
-			res.redirect('/survey-notavailable');
-		} finally {
-			// Check survey groups
-			await this.surveyGroupCheckAndSurveyAttemptEntry(
-				surveyNumber, 
-				survey.original_json.survey_group_ids, 
-				member.id
-			);
-		}
-	};
-
-	/**
-	 * Rebuild the entry link with hash
-	 * NOTE: When hashing URLs the base string should include the entire URL, up to and including the `&` preceding the hashing parameter.
-	 * For more details visit https://partners.lucidhq.com/s/security-sha-1-hashing
-	 */
-	rebuildEntryLink = (url, queryParams) => {
-		if (process.env.DEV_MODE == '0') {
-			// 0 = live
-			var url = url.replace('&PID=', '');
-			const params = new URLSearchParams(queryParams).toString();
-			const urlTobeHashed = url + '&' + params + '&';
-			const hash = generateHashForLucid(urlTobeHashed);
-			return url + '&' + params + '&hash=' + hash;
-		} else {
-			delete queryParams.PID;
-			const params = new URLSearchParams(queryParams).toString();
-			const urlTobeHashed = url + '&' + params + '&';
-			const hash = generateHashForLucid(urlTobeHashed);
-			return url + '&' + params + '&hash=' + hash;
-		}
-	};
-
-	addEntryLink = async (surveyNumber, url) => {
-		await sequelize.query(
-			'UPDATE surveys SET url = :url WHERE survey_number = :survey_number AND survey_provider_id = :provider_id',
-			{
-				replacements: { url: url, survey_number: surveyNumber, provider_id: 1 },
-				type: QueryTypes.UPDATE,
-			}
-		);
-		return true;
-	};
-
-	generateQueryString = (queryString, qualifications, eligibilities) => {
-		let quesStr = {};
-		let limit = 5; // 5 is set because Lucid have a limitation to set the querystring in the URL.
-		let countStr = Object.keys(queryString).length;
-		for (let qual of qualifications) {
-			let findEl = eligibilities.find(
-				(el) => el.survey_question_id == qual.survey_question_id
-			);
-			if (findEl !== undefined) {
-				quesStr[findEl.survey_provider_question_id] = findEl.option;
-			}
-
-			if (Object.keys(quesStr).length + countStr === limit) {
-				break;
-			}
-		}
-		return new URLSearchParams({ ...queryString, ...quesStr }).toString();
-	};
-
-	/**
-	 * Check survey group and inserted all the group surveys to survey_attempt table
-	 */
-	surveyGroupCheckAndSurveyAttemptEntry = async(surveyNumber, surveyGroupId, memberId) => {
-		try {
-			const lcObj = new LucidHelper();
-			const getData = await lcObj.getSurveyGroups(surveyNumber);
-			const groupIds = [surveyNumber];
-			if (getData.ResultCount != 0 && getData.SurveyGroups.length) {				
-				getData.SurveyGroups.forEach(row => {
-					if (surveyGroupId.includes(row.SurveyGroupID)) {
-						groupIds.push(...groupIds, row.SurveyGroupID, ...row.SurveyGroupSurveys);
-					}
-				});				
-			}
-			if (groupIds.length) {
-				const params = [];
-				groupIds.forEach(row => {
-					params.push({
-						survey_number: row,
-						member_id: memberId,
-						survey_provider_id: 1
-					});
-				});
-				await SurveyAttempt.bulkCreate(params, {
-					ignoreDuplicates: true
-				});
-			}
-		}
-		catch (error) { }
-
-		return;
-	}
+	};*/
 }
 
 module.exports = LucidController;
