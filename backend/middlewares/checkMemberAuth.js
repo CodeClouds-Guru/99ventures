09f1e45f40bdf8efe428987ffb9adf640eb0bd4e
debosmitadey <debosmita.dey@codeclouds.co.in>
2024-11-05 17:44:00 +0530

    commit for alert change for soft deleted account7

--- backend/middlewares/checkMemberAuth.js
+++ backend/middlewares/checkMemberAuth.js
@@ -54,12 +54,9 @@ module.exports = async function (req, res, next) {
       ],
     });
     if (!member) {
-      req.session.flash = {
-        error:
-          'Your account has been suspended. Please contact our support team via support ticket. You will find this in our "help" tab.',
-      };
-      res.status(401).redirect('/faq');
-      return;
+      req.session.member = null;
+      req.session.impersonation = null;
+      res.redirect('/');
     }
     // console.log('member', member);
     //get total earnings
